4521d9ac59ba1b3501f6d8281152d12f51ed17bc
Łukasz Kujawski <leon@wildland.io>
2022-09-20 13:19:13 +0200

    proposal formatting

--- env.ts
+++ env.ts
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const PROPOSALS_BASE_URI = process.env.PROPOSALS_BASE_URI || 'https://ipfs.io/ipfs/QmVtPWVpJUNXeuHgaKLhLt8ZJzmoRZMDrKsorDeFg2Nhr6/';
+const PROPOSALS_BASE_URI = process.env.PROPOSALS_BASE_URI || 'https://ipfs.io/ipfs/QmPQM71PbBHDTwP7Vsg9jcLQNxtGnRe3NLnnNJgoQXgBDd/';
 
 export {
   PROPOSALS_BASE_URI
