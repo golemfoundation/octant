cb685b8c7f54d9c481cc7ff613d126e3857f2bb3
Łukasz Kujawski <leon@wildland.io>
2022-09-26 14:05:17 +0200

    add Solidity linter

--- contracts/Proposals.sol
+++ contracts/Proposals.sol
@@ -6,16 +6,15 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Proposals is Ownable {
-
     struct Proposal {
         uint8 id;
         string uri;
     }
 
-    string baseURI;
-    uint8 proposalsCount;
+    string public baseURI;
+    uint8 public proposalsCount;
 
-    constructor(string memory _initBaseURI, uint8 _proposalsCount) {
+    constructor(string memory _initBaseURI, uint8 _proposalsCount) public {
         setBaseURI(_initBaseURI);
         setProposalsCount(_proposalsCount);
     }
@@ -31,7 +30,9 @@ contract Proposals is Ownable {
     function getProposals() external view returns (Proposal[] memory) {
         Proposal[] memory proposals = new Proposal[](proposalsCount);
         for (uint8 i = 0; i < proposalsCount; i++) {
-            string memory uri = string(abi.encodePacked(baseURI, Strings.toString(i)));
+            string memory uri = string(
+                abi.encodePacked(baseURI, Strings.toString(i))
+            );
             proposals[i] = Proposal(i, uri);
         }
         return proposals;
