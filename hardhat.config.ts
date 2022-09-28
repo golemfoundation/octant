00414e7aae09ce51aeb766cb63814fe3b1e8624b
Łukasz Kujawski <leon@wildland.io>
2022-09-28 09:47:14 +0200

    GE-29: resolve conflicts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,23 +1,21 @@
-import { HardhatUserConfig } from "hardhat/types";
-import "hardhat-deploy";
-import "hardhat-deploy-ethers";
-import "@matterlabs/hardhat-zksync-deploy";
-import "@matterlabs/hardhat-zksync-solc";
+import { HardhatUserConfig } from 'hardhat/types';
+import 'hardhat-deploy';
+import 'hardhat-deploy-ethers';
+import '@matterlabs/hardhat-zksync-deploy';
+import '@matterlabs/hardhat-zksync-solc';
+import '@typechain/hardhat';
 
 import { ETHERSCAN_API_KEY, GOERLI_PRIVATE_KEY, GOERLI_URL, ZKSYNC_URL } from './env';
-import '@typechain/hardhat'
-
-import { ETHERSCAN_API_KEY, GOERLI_PRIVATE_KEY, GOERLI_URL } from './env';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: '0.8.9',
   networks: {
     hardhat: {
       chainId: 1337,
       initialBaseFeePerGas: 0,
     },
     localhost: {
-      url: "http://127.0.0.1:8545",
+      url: 'http://127.0.0.1:8545',
       chainId: 1337,
       initialBaseFeePerGas: 0,
     },
@@ -40,6 +38,20 @@ const config: HardhatUserConfig = {
     etherscan: {
       apiKey: ETHERSCAN_API_KEY
     }
+  },
+  zksolc: {
+    version: '1.1.5',
+    compilerSource: 'docker',
+    settings: {
+      experimental: {
+        dockerImage: 'matterlabs/zksolc',
+        tag: 'v1.1.5'
+      },
+    },
+  },
+  zkSyncDeploy: {
+    zkSyncNetwork: ZKSYNC_URL,
+    ethNetwork: GOERLI_URL
   }
 };
 
