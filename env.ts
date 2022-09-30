4d8f11698dd98f18a3a6042287593914546a833c
Łukasz Kujawski <leon@wildland.io>
2022-09-30 07:29:55 +0200

    HOTFIX: fix proposals counting and add IPFS_GATEWAY

--- env.ts
+++ env.ts
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
-const PROPOSALS_BASE_URI = process.env.PROPOSALS_BASE_URI || 'https://ipfs.io/ipfs/QmPQM71PbBHDTwP7Vsg9jcLQNxtGnRe3NLnnNJgoQXgBDd/';
+const IPFS_GATEWAY = process.env.IPFS_GATEWAY || 'https://ipfs.io'
+const PROPOSALS_BASE_URI = process.env.PROPOSALS_BASE_URI || `${IPFS_GATEWAY}/ipfs/QmVFvqs93qrn5xayVuEcNwVg5CukSrVHUGJGra3zQsgMax/`;
 const GOERLI_URL = process.env.GOERLI_URL || '';
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || '0000000000000000000000000000000000000000000000000000000000000000';
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
