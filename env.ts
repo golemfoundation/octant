c063ee671a3111d337e0986ee9d00d7a6a3e7b63
Łukasz Kujawski <leon@wildland.io>
2022-10-05 17:20:46 +0200

    HOTFIX: fix epochs deployment

--- env.ts
+++ env.ts
@@ -7,7 +7,7 @@ const ZKSYNC_URL = process.env.ZKSYNC_URL || 'https://zksync2-testnet.zksync.dev
 const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || '0000000000000000000000000000000000000000000000000000000000000000';
 const GOERLI_GLM = process.env.GOERLI_GLM || '0x0393EfF8fD1C5B5CaBac9b2d437798B6792fe013'
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
-const EPOCHS_START = Number(process.env.EPOCHS_START) || Date.now() / 1000;
+const EPOCHS_START = Number(process.env.EPOCHS_START) || Math.round(Date.now() / 1000);
 const EPOCH_DURATION = Number(process.env.EPOCH_DURATION) || 300_000;
 const DECISION_WINDOW = Number(process.env.DECISION_WINDOW) || 120_000;
 
