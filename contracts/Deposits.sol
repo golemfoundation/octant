c5e7f6ed3164a23a789e2364f7a89c1903d664d2
Pawel Peregud <paulperegud@protonmail.com>
2022-10-12 14:26:09 +0200

    drop console.logs from contract

--- contracts/Deposits.sol
+++ contracts/Deposits.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.9;
 
 import "./interfaces/IEpochs.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "hardhat/console.sol";
 
 /*
  * Contract tracking GLM deposits (staking) for Hexagon project.
@@ -36,7 +35,6 @@ contract Deposits {
     }
 
     function deposit(uint256 amount) public {
-        console.log("CONTRACT deposit", epochs.getCurrentEpoch(), amount);
         uint256 current = deposits[msg.sender];
         deposits[msg.sender] = current + amount;
         uint256 epoch = epochs.getCurrentEpoch();
@@ -50,7 +48,6 @@ contract Deposits {
     }
 
     function withdraw(uint256 amount) public {
-        console.log("CONTRACT withdraw", epochs.getCurrentEpoch(),  amount);
         uint256 current = deposits[msg.sender];
         require(current >= amount, "HN/deposit-is-smaller");
         deposits[msg.sender] = current - amount;
@@ -61,7 +58,6 @@ contract Deposits {
     }
 
     function stakeAt(address owner, uint256 epochNo) public view returns (uint256) {
-        console.log("CONTRACT stakeAt", epochs.getCurrentEpoch(),  epochNo);
         uint256 currentEpoch = epochs.getCurrentEpoch();
         require(epochNo <= currentEpoch, "HN/future-is-unknown");
         require(epochNo > 0, "HN/epochs-start-from-1");
