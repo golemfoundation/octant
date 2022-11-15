e1fe9f7a3adbe69b7b27bb7898ca12609c8aac41
Łukasz Kujawski <lukas.kujawski@gmail.com>
2022-11-15 09:39:42 +0000

    GE-43: Oracle contract

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -7,3 +7,6 @@ export const DEPOSITS = "Deposits";
 export const TOKEN = "Token";
 export const ALLOCATIONS = "Allocations";
 export const EPOCHS = "Epochs";
+export const BEACON_CHAIN_ORACLE = "BeaconChainOracle";
+export const EXECUTION_LAYER_ORACLE = "ExecutionLayerOracle";
+export const HEXAGON_ORACLE = "HexagonOracle";
