42c5bbf2298e3fae2b3d5d300a93a1ea1f095923
Andrzej Ziółek <a.j.ziolek@gmail.com>
2022-11-17 13:00:00 +0100

    feat: proposals view prototype

---
+++ client/src/components/core/img/types.ts
@@ -0,0 +1,5 @@
+export default interface ImgProps {
+  alt?: string;
+  className?: string;
+  src: string;
+}
--- client/src/components/dedicated/proposal-item/proposal-item.component.tsx
+++ client/src/components/dedicated/proposal-item/proposal-item.component.tsx
@@ -2,6 +2,7 @@ import { useQuery } from 'react-query';
 import React, { FC, Fragment } from 'react';
 
 import Button from 'components/core/button/button.component';
+import Img from 'components/core/img/img.component';
 
 import { ProposalItemProps } from './types';
 import styles from './style.module.scss';
@@ -31,24 +32,48 @@ const ProposalItem: FC<ProposalItemProps> = ({
       {isLoadingError ? (
         'Loading of proposal encountered an error.'
       ) : (
-        // eslint-disable-next-line react/no-array-index-key
         <Fragment>
-          <div>{name}</div>
-          <div>{description}</div>
-          {vote && (
-            <div>
-              Number of votes: {numberOfVotes}.
-              <Button label="Vote" onClick={() => vote(id, 100)} />
+          <div className={styles.header}>
+            <div className={styles.imageLandscapeWrapper}>
+              <Img
+                className={styles.imageLandscape}
+                src="https://via.placeholder.com/600x300.jpg/0000FF/FFFFFF"
+              />
             </div>
-          )}
-          <div>
-            <Button href={website} label="Website" target="_blank" />
+            <Img
+              className={styles.imageProfile}
+              src="https://via.placeholder.com/64x64.jpg/000000/FFFFFF"
+            />
           </div>
-          <div className={styles.buttons}>
-            {socialLinks.map((link, buttonIndex) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <Button key={buttonIndex} href={link} label="Social link" target="_blank" />
-            ))}
+          <div className={styles.body}>
+            <div className={styles.name}>{name}</div>
+            <div className={styles.description}>{description}</div>
+          </div>
+          <div className={styles.footer}>
+            <div className={styles.numbers}>
+              <div className={styles.sum}>$5300</div>
+              <div className={styles.percentage}>32%</div>
+            </div>
+            <div>
+              <Button label="Add to Allocate" />
+            </div>
+          </div>
+          {/* Below section is to be removed completely */}
+          <div className={styles.debug}>
+            Debug section:
+            <div className={styles.buttons}>
+              {vote && (
+                <div>
+                  Number of votes: {numberOfVotes}.
+                  <Button label="Vote" onClick={() => vote(id, 100)} />
+                </div>
+              )}
+              <Button href={website} label="Website" target="_blank" />
+              {socialLinks.map((link, buttonIndex) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <Button key={buttonIndex} href={link} label="Social link" target="_blank" />
+              ))}
+            </div>
           </div>
         </Fragment>
       )}
