b5e139901ea9bfab24a3308146e36eca8d3117f1
Łukasz Kujawski <lukas.kujawski@gmail.com>
2022-12-12 11:38:55 +0000

    Hotfix/deployment pages

--- client/src/env.ts
+++ client/src/env.ts
@@ -2,17 +2,17 @@
 const env = {
   allocationsAddress:
     // @ts-expect-error TS does not understand the way vite imports envs.
-    import.meta.env.VITE_ALLOCATIONS_ADDRESS || '0x813B40572654B26B2d779829261D5219e5943723',
+    import.meta.env.VITE_ALLOCATIONS_ADDRESS || '0x18618fcE142B8d0278f4655D30551ADf71f8A05b',
   allocationsStorageAddress:
     // @ts-expect-error TS does not understand the way vite imports envs.
     import.meta.env.VITE_ALLOCATIONS_STORAGE_ADDRESS ||
-    '0x05c08404435b5DF86FbbD14B5d3BAAb91cE95588',
+    '0xa981f993300FE6D14D6Cf94EB8888445f918Ef77',
   depositsAddress:
     // @ts-expect-error TS does not understand the way vite imports envs.
-    import.meta.env.VITE_DEPOSITS_ADDRESS || '0x3c6ab3E060F8c9c07A9dAd0a0c2e02298e4a939f',
+    import.meta.env.VITE_DEPOSITS_ADDRESS || '0xec89E93141EAa4C8102EAC27b133b86a3d5940b0',
   epochsAddress:
     // @ts-expect-error TS does not understand the way vite imports envs.
-    import.meta.env.VITE_EPOCHS_ADDRESS || '0x3318e56BC1cF548420818732ce6C8e3C5BF3E304',
+    import.meta.env.VITE_EPOCHS_ADDRESS || '0x1a77Ac64E7bb8f91B85e2FA50Ab448bA4c3AB249',
   glmAddress:
     // @ts-expect-error TS does not understand the way vite imports envs.
     import.meta.env.VITE_GLM_ADDRESS || '0x4914bdFfFA96a3936304705eE41C2F5CbcAD7774',
@@ -21,10 +21,10 @@ const env = {
     import.meta.env.IPFS_GATEWAY || 'https://hexagon.infura-ipfs.io',
   proposalsAddress:
     // @ts-expect-error TS does not understand the way vite imports envs.
-    import.meta.env.VITE_PROPOSALS_ADDRESS || '0x52716631EAd76efA264039101Fb022bB063b1411',
+    import.meta.env.VITE_PROPOSALS_ADDRESS || '0xbA0c5F28ea1a313ccD6AbcE837B7acaa4691e329',
   rewardsAddress:
     // @ts-expect-error TS does not understand the way vite imports envs.
-    import.meta.env.VITE_REWARDS_ADDRESS || '0xB4756950a43f20F73Cda820eB79607FA0CD701d7',
+    import.meta.env.VITE_REWARDS_ADDRESS || '0x25B4B25a0C959127fc82b56f051b2EB43813F24F',
 };
 
 export default env;
