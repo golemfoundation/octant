a30d2a73b61054ff3dd44817b98e248a44f70b9c
Łukasz Kujawski <leon@wildland.io>
2022-12-13 16:04:59 +0100

    HEX-93: add hardhat task

--- contracts/TestGLMFaucet.sol
+++ contracts/TestGLMFaucet.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestGLMFaucet {
 
-    uint256 private constant AMOUNT = 1000 ether;
+    uint256 constant public AMOUNT = 1000 ether;
     uint256 constant public WAIT_TIME = 24 hours;
 
     /// @notice GLM token contract address
@@ -13,21 +13,27 @@ contract TestGLMFaucet {
 
     event Withdrawn(uint256 amount, uint256 when, address to);
 
-    mapping(address => uint256) nextAccessTime;
+    mapping(address => uint256) public nextAccessTime;
 
     /// @param glmAddress Address of TestGLM contract.
     constructor(address glmAddress) {
         glm = ERC20(glmAddress);
     }
 
-    function sendMeGLM() external allowedToGetGLM {
+    function sendMeGLM() external allowedToGetGLM(msg.sender) {
         require(glm.transfer(msg.sender, AMOUNT));
         nextAccessTime[msg.sender] = block.timestamp + WAIT_TIME;
         emit Withdrawn(AMOUNT, block.timestamp, msg.sender);
     }
 
-    modifier allowedToGetGLM() {
-        require(nextAccessTime[msg.sender] == 0 || nextAccessTime[msg.sender] < block.timestamp, "HN/must-wait-24-hours-since-last-withdraw");
+    function sendGLM(address _recipient) external allowedToGetGLM(_recipient) {
+        require(glm.transfer(_recipient, AMOUNT));
+        nextAccessTime[_recipient] = block.timestamp + WAIT_TIME;
+        emit Withdrawn(AMOUNT, block.timestamp, _recipient);
+    }
+
+    modifier allowedToGetGLM(address _recipient) {
+        require(nextAccessTime[_recipient] == 0 || nextAccessTime[_recipient] < block.timestamp, "HN/must-wait-24-hours-since-last-withdraw");
         _;
     }
 }
--- test/Faucet.test.ts
+++ test/Faucet.test.ts
@@ -9,34 +9,67 @@ makeTestsEnv(DEPOSITS, (testEnv) => {
   describe('Faucet', async () => {
 
     it('Can withdraw GLM', async () => {
-      const { token, faucet, signers } = testEnv;
-      await token.transfer(faucet.address, parseEther("10000"));
+      const { token, faucet, signers: { Alice } } = testEnv;
+      await token.transfer(faucet.address, parseEther('10000'));
 
-      await faucet.connect(signers.Alice).sendMeGLM();
+      await faucet.connect(Alice).sendMeGLM();
 
-      expect(await token.balanceOf(signers.Alice.address)).eq(parseEther("1000"));
+      expect(await token.balanceOf(Alice.address)).eq(parseEther('1000'));
     });
 
     it('Can\'t withdrawn twice in 24 hours', async () => {
-      const { token, faucet, signers } = testEnv;
-      await token.transfer(faucet.address, parseEther("10000"));
+      const { token, faucet, signers: { Alice } } = testEnv;
+      await token.transfer(faucet.address, parseEther('10000'));
 
-      await faucet.connect(signers.Alice).sendMeGLM();
+      await faucet.connect(Alice).sendMeGLM();
 
-      expect(faucet.connect(signers.Alice).sendMeGLM()).to.be.revertedWith(
+      expect(faucet.connect(Alice).sendMeGLM()).to.be.revertedWith(
         'HN/must-wait-24-hours-since-last-withdraw'
       );
     });
 
     it('Can withdrawn after 24 hours', async () => {
-      const { token, faucet, signers } = testEnv;
-      await token.transfer(faucet.address, parseEther("10000"));
+      const { token, faucet, signers: { Alice } } = testEnv;
+      await token.transfer(faucet.address, parseEther('10000'));
 
-      await faucet.connect(signers.Alice).sendMeGLM();
+      await faucet.connect(Alice).sendMeGLM();
       await increaseNextBlockTimestamp(86400);
-      await faucet.connect(signers.Alice).sendMeGLM();
+      await faucet.connect(Alice).sendMeGLM();
 
-      expect(await token.balanceOf(signers.Alice.address)).eq(parseEther("2000"));
+      expect(await token.balanceOf(Alice.address)).eq(parseEther('2000'));
+    });
+
+    it('Can send GLM to recipient', async () => {
+      const { token, faucet, signers: { Alice, Bob } } = testEnv;
+      await token.transfer(faucet.address, parseEther('10000'));
+
+      await faucet.connect(Alice).sendGLM(Bob.address);
+
+      expect(await token.balanceOf(Alice.address)).eq(0);
+      expect(await token.balanceOf(Bob.address)).eq(parseEther('1000'));
+    });
+
+    it('Can\'t send to recipient twice in 24 hours', async () => {
+      const { token, faucet, signers: { Alice, Bob } } = testEnv;
+      await token.transfer(faucet.address, parseEther('10000'));
+
+      await faucet.connect(Alice).sendGLM(Bob.address);
+
+      expect(faucet.connect(Alice).sendGLM(Bob.address)).to.be.revertedWith(
+        'HN/must-wait-24-hours-since-last-withdraw'
+      );
+    });
+
+    it('Can send to recipient after 24 hours', async () => {
+      const { token, faucet, signers: { Alice, Bob } } = testEnv;
+      await token.transfer(faucet.address, parseEther('10000'));
+
+      await faucet.connect(Alice).sendGLM(Bob.address);
+      await increaseNextBlockTimestamp(86400);
+      await faucet.connect(Alice).sendGLM(Bob.address);
+
+      expect(await token.balanceOf(Alice.address)).eq(0);
+      expect(await token.balanceOf(Bob.address)).eq(parseEther('2000'));
     });
   });
 });
