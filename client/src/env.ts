d678fa4a0eb5139c792bbc2f31615333482241dd
Andrzej Ziółek <a.j.ziolek@gmail.com>
2022-12-21 11:18:09 +0000

    FIX: Alignment w/ contracts, fetching of the vote for previous epoch

--- client/src/env.ts
+++ client/src/env.ts
@@ -2,17 +2,17 @@
 const env = {
   allocationsAddress:
     // @ts-expect-error TS does not understand the way vite imports envs.
-    import.meta.env.VITE_ALLOCATIONS_ADDRESS || '0x4267080A271a581F58E260D30c2bF3Ac7B67D1a8',
+    import.meta.env.VITE_ALLOCATIONS_ADDRESS || '0xB68546e70b36CC123d62a72280363f24EDd6B69B',
   allocationsStorageAddress:
     // @ts-expect-error TS does not understand the way vite imports envs.
     import.meta.env.VITE_ALLOCATIONS_STORAGE_ADDRESS ||
-    '0x62ED8a84f08465ab3FE02490B2Ad75248acb3386',
+    '0x02dad19f52b567440c5be7b03d6Dd74772704EEC',
   depositsAddress:
     // @ts-expect-error TS does not understand the way vite imports envs.
-    import.meta.env.VITE_DEPOSITS_ADDRESS || '0xE209338fA633B1d75430C053aaA2174eC3018d86',
+    import.meta.env.VITE_DEPOSITS_ADDRESS || '0xf1C2554C779Bc6C9A93106A280E986E33e7F4637',
   epochsAddress:
     // @ts-expect-error TS does not understand the way vite imports envs.
-    import.meta.env.VITE_EPOCHS_ADDRESS || '0xd5F0C8b48A3C6577F6120c0D2e347d05b2bB868f',
+    import.meta.env.VITE_EPOCHS_ADDRESS || '0x30b26116C6b2B0Cf8f382525Bb15771572975B67',
   glmAddress:
     // @ts-expect-error TS does not understand the way vite imports envs.
     import.meta.env.VITE_GLM_ADDRESS || '0x4914bdFfFA96a3936304705eE41C2F5CbcAD7774',
@@ -21,10 +21,10 @@ const env = {
     import.meta.env.IPFS_GATEWAY || 'https://hexagon.infura-ipfs.io',
   proposalsAddress:
     // @ts-expect-error TS does not understand the way vite imports envs.
-    import.meta.env.VITE_PROPOSALS_ADDRESS || '0xd6DFA539A11aB5a31fb150Bb79ce9309A7614557',
+    import.meta.env.VITE_PROPOSALS_ADDRESS || '0x7C2e14004836CDAaD223727cD37df282E8467aF1',
   rewardsAddress:
     // @ts-expect-error TS does not understand the way vite imports envs.
-    import.meta.env.VITE_REWARDS_ADDRESS || '0x2401C99A46567a60d8DC9B5Fe5425d616aA558Be',
+    import.meta.env.VITE_REWARDS_ADDRESS || '0x69e4cAEca72fA1ce881faac35D849B18c46d9345',
 };
 
 export default env;
