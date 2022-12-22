fd54916a861c4489a52bcad306c3732bbae092dd
Andrzej Ziółek <a.j.ziolek@gmail.com>
2022-12-22 18:02:44 +0000

    HEX-103 Implement empty states

--- client/src/views/earn-view/utils.ts
+++ client/src/views/earn-view/utils.ts
@@ -2,21 +2,13 @@ import { BigNumberish } from 'ethers';
 import { formatUnits } from 'ethers/lib/utils';
 
 interface GetCurrentEpochStateTextParams {
-  isConnected: boolean;
   suffix: string;
   value?: BigNumberish;
 }
 
 export function getCurrentEpochStateText({
-  isConnected,
   value,
   suffix,
 }: GetCurrentEpochStateTextParams): string {
-  if (!isConnected) {
-    return 'Please connect your wallet first.';
-  }
-  if (value === undefined) {
-    return 'Fetching data...';
-  }
-  return `${formatUnits(value)} ${suffix}`;
+  return `${formatUnits(value || 0)} ${suffix}`;
 }
