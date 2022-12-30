0e7017c4094314a9235550e55ee4d6687138a071
Andrzej Ziółek <a.j.ziolek@gmail.com>
2022-12-30 10:05:29 +0000

    HEX-138 List of donors -- mapping of response

--- client/src/hooks/useProjectDonors.ts
+++ client/src/hooks/useProjectDonors.ts
@@ -1,12 +1,13 @@
+/* eslint-disable */
 import { BigNumber } from 'ethers';
 import { UseQueryResult, useQuery } from 'react-query';
 
 import useContractAllocationsStorage from './contracts/useContractAllocationsStorage';
 import useCurrentEpoch from './useCurrentEpoch';
 
-export default function useGetUsersAlphas(
-  proposalId: string,
-): UseQueryResult<[string[], BigNumber[]]> {
+type Donor = { alpha: BigNumber; address: string };
+
+export default function useProjectDonors(proposalId: string): UseQueryResult<Donor[]> {
   const contractAllocationsStorage = useContractAllocationsStorage();
   const { data: currentEpoch } = useCurrentEpoch();
 
@@ -15,6 +16,11 @@ export default function useGetUsersAlphas(
     () => contractAllocationsStorage?.getUsersAlphas(currentEpoch! - 1, proposalId),
     {
       enabled: !!contractAllocationsStorage && !!currentEpoch,
+      select: response =>
+        response![0].map((address, index) => ({
+          address,
+          alpha: response![1][index],
+        })),
     },
   );
 }
