ef049ca15db8bfb5f512453023beb69b189be5a5
Andrzej Ziółek <a.j.ziolek@gmail.com>
2023-01-13 18:54:20 +0000

    [HOTFIX] Rounding of ETH values missing 1 whole power

--- client/src/utils/getFormattedUnit.ts
+++ client/src/utils/getFormattedUnit.ts
@@ -3,16 +3,16 @@ import { formatUnits } from 'ethers/lib/utils';
 
 import { dotAndZeroes } from './regExp';
 
-const GWEI_4 = BigNumber.from(10).pow(13);
-const WEI_4 = BigNumber.from(10).pow(4);
+const GWEI_5 = BigNumber.from(10).pow(14);
+const WEI_5 = BigNumber.from(10).pow(5);
 
 export default function getFormattedUnits(value: BigNumber): string {
   let returnValue: { suffix: string; value: string };
   if (value.isZero()) {
     returnValue = { suffix: 'ETH', value: formatUnits(value) };
-  } else if (value.lte(WEI_4)) {
+  } else if (value.lt(WEI_5)) {
     returnValue = { suffix: 'WEI', value: formatUnits(value, 'wei') };
-  } else if (value.lte(GWEI_4)) {
+  } else if (value.lt(GWEI_5)) {
     returnValue = { suffix: 'GWEI', value: formatUnits(value, 'gwei') };
   } else {
     returnValue = { suffix: 'ETH', value: formatUnits(value) };
