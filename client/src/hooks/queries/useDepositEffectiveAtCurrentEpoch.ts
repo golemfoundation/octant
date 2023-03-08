432c128322c321f01abf5ae2cb183bd69e431f43
Andrzej Ziółek <a.j.ziolek@gmail.com>
2023-03-08 13:08:45 +0000

    [HOTFIX] Current effective deposit should not prevent allocation -- check previous epoch effective deposit

--- client/src/hooks/queries/useDepositEffectiveAtCurrentEpoch.ts
+++ client/src/hooks/queries/useDepositEffectiveAtCurrentEpoch.ts
@@ -1,21 +1,11 @@
 import { BigNumber } from 'ethers';
-import { UseQueryResult, useQuery } from 'react-query';
-import { useMetamask } from 'use-metamask';
-
-import useContractTracker from 'hooks/contracts/useContractTracker';
+import { UseQueryResult } from 'react-query';
 
 import useCurrentEpoch from './useCurrentEpoch';
+import useDepositEffectiveAtGivenEpoch from './useDepositEffectiveAtGivenEpoch';
 
 export default function useDepositEffectiveAtCurrentEpoch(): UseQueryResult<BigNumber | undefined> {
-  const {
-    metaState: { web3, account },
-  } = useMetamask();
-  const signer = web3?.getSigner();
-  const address = account[0];
-  const contractTracker = useContractTracker({ signerOrProvider: signer });
   const { data: currentEpoch } = useCurrentEpoch();
 
-  return useQuery(['depositAt'], () => contractTracker?.depositAt(address, currentEpoch!), {
-    enabled: !!contractTracker && !!currentEpoch && !!address,
-  });
+  return useDepositEffectiveAtGivenEpoch(currentEpoch);
 }
