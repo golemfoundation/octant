2de5e9b2f3057465047eef73147c6746bcd8b5b5
Jakub Mikołajczyk <jakub@wildland.io>
2023-05-31 08:48:11 +0000

    OCT-492 cypress - run tests for all breakpoints

--- client/cypress/e2e/onboarding.cy.ts
+++ client/cypress/e2e/onboarding.cy.ts
@@ -1,4 +1,5 @@
 import { checkLocationWithLoader, visitWithLoader } from 'cypress/utils/e2e';
+import viewports from 'cypress/utils/viewports';
 import { ROOT, ROOT_ROUTES } from 'src/routes/RootRoutes/routes';
 import steps from 'src/views/OnboardingView/steps';
 
@@ -15,41 +16,43 @@ const checkProgressStepperSlimIsCurrentAndClickNext = (index): Chainable<any> =>
     .click();
 };
 
-describe('onboarding', () => {
-  beforeEach(() => {
-    cy.clearLocalStorage();
-    visitWithLoader(ROOT.absolute, ROOT_ROUTES.onboarding.absolute);
-  });
+Object.values(viewports).forEach(({ device, viewportWidth, viewportHeight }) => {
+  describe(`onboarding: ${device}`, { viewportHeight, viewportWidth }, () => {
+    beforeEach(() => {
+      cy.clearLocalStorage();
+      visitWithLoader(ROOT.absolute, ROOT_ROUTES.onboarding.absolute);
+    });
 
-  it('user is able to click through entire onboarding flow', () => {
-    for (let i = 0; i < steps.length - 1; i++) {
-      checkProgressStepperSlimIsCurrentAndClickNext(i);
-    }
+    it('user is able to click through entire onboarding flow', () => {
+      for (let i = 0; i < steps.length - 1; i++) {
+        checkProgressStepperSlimIsCurrentAndClickNext(i);
+      }
 
-    cy.get('[data-test=OnboardingView__ProgressStepperSlim__element]')
-      .eq(steps.length - 1)
-      .click();
-    cy.get('[data-test=ProposalsView__List]').should('be.visible');
-  });
+      cy.get('[data-test=OnboardingView__ProgressStepperSlim__element]')
+        .eq(steps.length - 1)
+        .click();
+      cy.get('[data-test=ProposalsView__List]').should('be.visible');
+    });
 
-  it('user is able to close the modal by clicking button in the top-right', () => {
-    cy.get('[data-test=OnboardingView__Modal__Button]').click();
-    cy.get('[data-test=ProposalsView__List]').should('be.visible');
-  });
+    it('user is able to close the modal by clicking button in the top-right', () => {
+      cy.get('[data-test=OnboardingView__Modal__Button]').click();
+      cy.get('[data-test=ProposalsView__List]').should('be.visible');
+    });
 
-  it('renders every time page is refreshed when "Always show Allocate onboarding" option is checked', () => {
-    cy.get('[data-test=OnboardingView__Modal__Button]').click();
-    cy.get('[data-test=Settings__Button]').click();
-    cy.get('[data-test=AlwaysShowOnboarding__InputCheckbox]').check().should('be.checked');
-    cy.reload();
-    checkLocationWithLoader(ROOT_ROUTES.onboarding.absolute);
-  });
+    it('renders every time page is refreshed when "Always show Allocate onboarding" option is checked', () => {
+      cy.get('[data-test=OnboardingView__Modal__Button]').click();
+      cy.get('[data-test=Settings__Button]').click();
+      cy.get('[data-test=AlwaysShowOnboarding__InputCheckbox]').check().should('be.checked');
+      cy.reload();
+      checkLocationWithLoader(ROOT_ROUTES.onboarding.absolute);
+    });
 
-  it('renders only once when "Always show Allocate onboarding" option is not checked', () => {
-    cy.get('[data-test=OnboardingView__Modal__Button]').click();
-    cy.get('[data-test=Settings__Button]').click();
-    cy.get('[data-test=AlwaysShowOnboarding__InputCheckbox]').should('not.be.checked');
-    cy.reload();
-    checkLocationWithLoader(ROOT_ROUTES.settings.absolute);
+    it('renders only once when "Always show Allocate onboarding" option is not checked', () => {
+      cy.get('[data-test=OnboardingView__Modal__Button]').click();
+      cy.get('[data-test=Settings__Button]').click();
+      cy.get('[data-test=AlwaysShowOnboarding__InputCheckbox]').should('not.be.checked');
+      cy.reload();
+      checkLocationWithLoader(ROOT_ROUTES.settings.absolute);
+    });
   });
 });
--- client/cypress/e2e/settings.cy.ts
+++ client/cypress/e2e/settings.cy.ts
@@ -1,4 +1,5 @@
 import { visitWithLoader } from 'cypress/utils/e2e';
+import viewports from 'cypress/utils/viewports';
 import { FIAT_CURRENCIES_SYMBOLS, DISPLAY_CURRENCIES } from 'src/constants/currencies';
 import {
   DISPLAY_CURRENCY,
@@ -10,124 +11,126 @@ import { ETH_STAKED } from 'src/constants/stake';
 import { ROOT_ROUTES } from 'src/routes/RootRoutes/routes';
 import getValueCryptoToDisplay from 'src/utils/getValueCryptoToDisplay';
 
-describe('settings', () => {
-  beforeEach(() => {
-    localStorage.setItem(IS_ONBOARDING_ALWAYS_VISIBLE, 'false');
-    localStorage.setItem(IS_ONBOARDING_DONE, 'true');
-    visitWithLoader(ROOT_ROUTES.settings.absolute);
-  });
-
-  it('"Always show Allocate onboarding" option toggle works', () => {
-    cy.get('[data-test=AlwaysShowOnboarding__InputCheckbox]').check();
-    cy.get('[data-test=AlwaysShowOnboarding__InputCheckbox]').should('be.checked');
-    cy.getAllLocalStorage().then(() => {
-      expect(localStorage.getItem(IS_ONBOARDING_ALWAYS_VISIBLE)).eq('true');
+Object.values(viewports).forEach(({ device, viewportWidth, viewportHeight }) => {
+  describe(`settings: ${device}`, { viewportHeight, viewportWidth }, () => {
+    beforeEach(() => {
+      localStorage.setItem(IS_ONBOARDING_ALWAYS_VISIBLE, 'false');
+      localStorage.setItem(IS_ONBOARDING_DONE, 'true');
+      visitWithLoader(ROOT_ROUTES.settings.absolute);
     });
 
-    cy.get('[data-test=AlwaysShowOnboarding__InputCheckbox]').click();
-    cy.get('[data-test=AlwaysShowOnboarding__InputCheckbox]').should('not.be.checked');
-    cy.getAllLocalStorage().then(() => {
-      expect(localStorage.getItem(IS_ONBOARDING_ALWAYS_VISIBLE)).eq('false');
+    it('"Always show Allocate onboarding" option toggle works', () => {
+      cy.get('[data-test=AlwaysShowOnboarding__InputCheckbox]').check();
+      cy.get('[data-test=AlwaysShowOnboarding__InputCheckbox]').should('be.checked');
+      cy.getAllLocalStorage().then(() => {
+        expect(localStorage.getItem(IS_ONBOARDING_ALWAYS_VISIBLE)).eq('true');
+      });
+
+      cy.get('[data-test=AlwaysShowOnboarding__InputCheckbox]').click();
+      cy.get('[data-test=AlwaysShowOnboarding__InputCheckbox]').should('not.be.checked');
+      cy.getAllLocalStorage().then(() => {
+        expect(localStorage.getItem(IS_ONBOARDING_ALWAYS_VISIBLE)).eq('false');
+      });
+
+      cy.get('[data-test=AlwaysShowOnboarding__InputCheckbox]').click();
+      cy.get('[data-test=AlwaysShowOnboarding__InputCheckbox]').should('be.checked');
+      cy.getAllLocalStorage().then(() => {
+        expect(localStorage.getItem(IS_ONBOARDING_ALWAYS_VISIBLE)).eq('true');
+      });
     });
 
-    cy.get('[data-test=AlwaysShowOnboarding__InputCheckbox]').click();
-    cy.get('[data-test=AlwaysShowOnboarding__InputCheckbox]').should('be.checked');
-    cy.getAllLocalStorage().then(() => {
-      expect(localStorage.getItem(IS_ONBOARDING_ALWAYS_VISIBLE)).eq('true');
-    });
-  });
-
-  it('"Use crypto as main value display" option is checked by default', () => {
-    cy.get('[data-test=UseCryptoAsMainValueDisplay__InputCheckbox]').should('be.checked');
-    cy.getAllLocalStorage().then(() => {
-      expect(localStorage.getItem(IS_CRYPTO_MAIN_VALUE_DISPLAY)).eq('true');
-    });
-  });
-
-  it('"Use crypto as main value display" option toggle works', () => {
-    cy.get('[data-test=UseCryptoAsMainValueDisplay__InputCheckbox]').check();
-    cy.get('[data-test=UseCryptoAsMainValueDisplay__InputCheckbox]').should('be.checked');
-    cy.getAllLocalStorage().then(() => {
-      expect(localStorage.getItem(IS_CRYPTO_MAIN_VALUE_DISPLAY)).eq('true');
+    it('"Use crypto as main value display" option is checked by default', () => {
+      cy.get('[data-test=UseCryptoAsMainValueDisplay__InputCheckbox]').should('be.checked');
+      cy.getAllLocalStorage().then(() => {
+        expect(localStorage.getItem(IS_CRYPTO_MAIN_VALUE_DISPLAY)).eq('true');
+      });
     });
 
-    cy.get('[data-test=UseCryptoAsMainValueDisplay__InputCheckbox]').click();
-    cy.get('[data-test=UseCryptoAsMainValueDisplay__InputCheckbox]').should('not.be.checked');
-    cy.getAllLocalStorage().then(() => {
-      expect(localStorage.getItem(IS_CRYPTO_MAIN_VALUE_DISPLAY)).eq('false');
+    it('"Use crypto as main value display" option toggle works', () => {
+      cy.get('[data-test=UseCryptoAsMainValueDisplay__InputCheckbox]').check();
+      cy.get('[data-test=UseCryptoAsMainValueDisplay__InputCheckbox]').should('be.checked');
+      cy.getAllLocalStorage().then(() => {
+        expect(localStorage.getItem(IS_CRYPTO_MAIN_VALUE_DISPLAY)).eq('true');
+      });
+
+      cy.get('[data-test=UseCryptoAsMainValueDisplay__InputCheckbox]').click();
+      cy.get('[data-test=UseCryptoAsMainValueDisplay__InputCheckbox]').should('not.be.checked');
+      cy.getAllLocalStorage().then(() => {
+        expect(localStorage.getItem(IS_CRYPTO_MAIN_VALUE_DISPLAY)).eq('false');
+      });
+
+      cy.get('[data-test=UseCryptoAsMainValueDisplay__InputCheckbox]').click();
+      cy.get('[data-test=UseCryptoAsMainValueDisplay__InputCheckbox]').should('be.checked');
+      cy.getAllLocalStorage().then(() => {
+        expect(localStorage.getItem(IS_CRYPTO_MAIN_VALUE_DISPLAY)).eq('true');
+      });
     });
 
-    cy.get('[data-test=UseCryptoAsMainValueDisplay__InputCheckbox]').click();
-    cy.get('[data-test=UseCryptoAsMainValueDisplay__InputCheckbox]').should('be.checked');
-    cy.getAllLocalStorage().then(() => {
-      expect(localStorage.getItem(IS_CRYPTO_MAIN_VALUE_DISPLAY)).eq('true');
-    });
-  });
-
-  it('"Use crypto as main value display" option by default displays crypto value as primary in DoubleValue component', () => {
-    cy.get('[data-test=Metrics__Button]').click();
-
-    const cryptoValue = getValueCryptoToDisplay({
-      cryptoCurrency: 'ethereum',
-      valueCrypto: ETH_STAKED,
-    });
-
-    cy.get('[data-test=MetricsView__DoubleValue--ethStaked]').within(() => {
-      cy.get('[data-test=MetricsView__DoubleValue--ethStaked__primary]')
-        .invoke('text')
-        .should('eq', cryptoValue);
-      cy.get('[data-test=MetricsView__DoubleValue--ethStaked__secondary]')
-        .invoke('text')
-        .should('not.eq', cryptoValue);
-    });
-  });
-
-  it('"Use crypto as main value display" option changes DoubleValue sections order', () => {
-    cy.get('[data-test=UseCryptoAsMainValueDisplay__InputCheckbox]').uncheck();
-    cy.get('[data-test=Metrics__Button]').click();
-
-    const cryptoValue = getValueCryptoToDisplay({
-      cryptoCurrency: 'ethereum',
-      valueCrypto: ETH_STAKED,
-    });
-
-    cy.get('[data-test=MetricsView__DoubleValue--ethStaked]').within(() => {
-      cy.get('[data-test=MetricsView__DoubleValue--ethStaked__primary]')
-        .invoke('text')
-        .should('not.eq', cryptoValue);
-      cy.get('[data-test=MetricsView__DoubleValue--ethStaked__secondary]')
-        .invoke('text')
-        .should('eq', cryptoValue);
-    });
-  });
-
-  it('"Choose a display currency" option works', () => {
-    cy.getAllLocalStorage().then(() => {
-      expect(localStorage.getItem(DISPLAY_CURRENCY)).eq('"usd"');
-    });
-
-    for (let i = 0; i < DISPLAY_CURRENCIES.length - 1; i++) {
-      const displayCurrency = DISPLAY_CURRENCIES[i];
-      const displayCurrencyToUppercase = displayCurrency.toUpperCase();
-      const nextDisplayCurrencyToUppercase =
-        i < DISPLAY_CURRENCIES.length - 1 ? DISPLAY_CURRENCIES[i + 1].toUpperCase() : undefined;
-
-      cy.get('[data-test=InputSelect__CustomSingleValue]').contains(displayCurrencyToUppercase);
+    it('"Use crypto as main value display" option by default displays crypto value as primary in DoubleValue component', () => {
       cy.get('[data-test=Metrics__Button]').click();
 
-      if (FIAT_CURRENCIES_SYMBOLS[displayCurrency]) {
-        cy.get('[data-test=MetricsView__DoubleValue--ethStaked__secondary]').contains(
-          FIAT_CURRENCIES_SYMBOLS[displayCurrency],
-        );
-      } else {
-        cy.get('[data-test=MetricsView__DoubleValue--ethStaked__secondary]').contains(
-          displayCurrencyToUppercase,
-        );
-      }
+      const cryptoValue = getValueCryptoToDisplay({
+        cryptoCurrency: 'ethereum',
+        valueCrypto: ETH_STAKED,
+      });
 
-      cy.get('[data-test=Settings__Button]').click();
-      cy.get('[data-test=InputSelect]').click();
-      cy.get(`[data-test=InputSelect__CustomOption--${nextDisplayCurrencyToUppercase}]`).click();
-    }
+      cy.get('[data-test=MetricsView__DoubleValue--ethStaked]').within(() => {
+        cy.get('[data-test=MetricsView__DoubleValue--ethStaked__primary]')
+          .invoke('text')
+          .should('eq', cryptoValue);
+        cy.get('[data-test=MetricsView__DoubleValue--ethStaked__secondary]')
+          .invoke('text')
+          .should('not.eq', cryptoValue);
+      });
+    });
+
+    it('"Use crypto as main value display" option changes DoubleValue sections order', () => {
+      cy.get('[data-test=UseCryptoAsMainValueDisplay__InputCheckbox]').uncheck();
+      cy.get('[data-test=Metrics__Button]').click();
+
+      const cryptoValue = getValueCryptoToDisplay({
+        cryptoCurrency: 'ethereum',
+        valueCrypto: ETH_STAKED,
+      });
+
+      cy.get('[data-test=MetricsView__DoubleValue--ethStaked]').within(() => {
+        cy.get('[data-test=MetricsView__DoubleValue--ethStaked__primary]')
+          .invoke('text')
+          .should('not.eq', cryptoValue);
+        cy.get('[data-test=MetricsView__DoubleValue--ethStaked__secondary]')
+          .invoke('text')
+          .should('eq', cryptoValue);
+      });
+    });
+
+    it('"Choose a display currency" option works', () => {
+      cy.getAllLocalStorage().then(() => {
+        expect(localStorage.getItem(DISPLAY_CURRENCY)).eq('"usd"');
+      });
+
+      for (let i = 0; i < DISPLAY_CURRENCIES.length - 1; i++) {
+        const displayCurrency = DISPLAY_CURRENCIES[i];
+        const displayCurrencyToUppercase = displayCurrency.toUpperCase();
+        const nextDisplayCurrencyToUppercase =
+          i < DISPLAY_CURRENCIES.length - 1 ? DISPLAY_CURRENCIES[i + 1].toUpperCase() : undefined;
+
+        cy.get('[data-test=InputSelect__CustomSingleValue]').contains(displayCurrencyToUppercase);
+        cy.get('[data-test=Metrics__Button]').click();
+
+        if (FIAT_CURRENCIES_SYMBOLS[displayCurrency]) {
+          cy.get('[data-test=MetricsView__DoubleValue--ethStaked__secondary]').contains(
+            FIAT_CURRENCIES_SYMBOLS[displayCurrency],
+          );
+        } else {
+          cy.get('[data-test=MetricsView__DoubleValue--ethStaked__secondary]').contains(
+            displayCurrencyToUppercase,
+          );
+        }
+
+        cy.get('[data-test=Settings__Button]').click();
+        cy.get('[data-test=InputSelect]').click();
+        cy.get(`[data-test=InputSelect__CustomOption--${nextDisplayCurrencyToUppercase}]`).click();
+      }
+    });
   });
 });
