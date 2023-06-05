46d286c86b8da32187360cb05e87b3b53de9c916
Andrzej Ziółek <a.j.ziolek@gmail.com>
2023-06-05 07:13:50 +0000

    OCT-522 Fix modal's dropping below the view when changing desktop to mobile viewport

--- client/src/components/dedicated/TipTile/TipTile.tsx
+++ client/src/components/dedicated/TipTile/TipTile.tsx
@@ -9,30 +9,28 @@ import { cross, info } from 'svg/misc';
 import styles from './TipTile.module.scss';
 import { TipTileProps } from './types';
 
-const TipTile: React.FC<TipTileProps> = ({ onClose, infoLabel, title, text, image, className }) => {
-  return (
-    <div className={cx(styles.root, className)}>
-      <div>
-        <div className={styles.info}>
-          <Svg img={info} size={3.2} />
-          <div className={styles.infoLabel}>{infoLabel}</div>
-        </div>
-        <div className={styles.body}>
-          <div className={styles.title}>{title}</div>
-          <div className={styles.text}>{text}</div>
-        </div>
+const TipTile: React.FC<TipTileProps> = ({ onClose, infoLabel, title, text, image, className }) => (
+  <div className={cx(styles.root, className)}>
+    <div>
+      <div className={styles.info}>
+        <Svg img={info} size={3.2} />
+        <div className={styles.infoLabel}>{infoLabel}</div>
       </div>
-      <div className={styles.imageWrapper}>
-        <Img className={styles.image} src={image} />
+      <div className={styles.body}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.text}>{text}</div>
       </div>
-      <Button
-        className={styles.buttonClose}
-        Icon={<Svg img={cross} size={1} />}
-        onClick={onClose}
-        variant="iconOnly"
-      />
     </div>
-  );
-};
+    <div className={styles.imageWrapper}>
+      <Img className={styles.image} src={image} />
+    </div>
+    <Button
+      className={styles.buttonClose}
+      Icon={<Svg img={cross} size={1} />}
+      onClick={onClose}
+      variant="iconOnly"
+    />
+  </div>
+);
 
 export default TipTile;
