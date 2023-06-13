8d6ffb71e44d1a0bf8e440160b016adb3a975b80
Jakub Mikołajczyk <jakub@wildland.io>
2023-06-13 12:21:21 +0000

    OCT-521 total effective deposit endpoint

--- backend/app/infrastructure/routes/__init__.py
+++ backend/app/infrastructure/routes/__init__.py
@@ -3,7 +3,7 @@ from flask_restx import Resource
 
 from app import settings
 from app.extensions import api
-from . import docs, history, epochs, rewards
+from . import docs, history, epochs, rewards, deposits
 
 
 @api.route("/favicon.ico")
