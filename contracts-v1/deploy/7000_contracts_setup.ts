e24c216e6b28eeab17380b9f6f4dc55b99037aca
Łukasz Kujawski <lukas.kujawski@gmail.com>
2023-06-14 16:22:48 +0000

    ci: fix contract deployment

--- contracts-v1/deploy/7000_contracts_setup.ts
+++ contracts-v1/deploy/7000_contracts_setup.ts
@@ -1,8 +1,8 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 
-import { AUTH } from '../helpers/constants';
-import { Auth } from '../typechain';
+import { AUTH, EPOCHS, PROPOSALS } from '../helpers/constants';
+import { Auth, Epochs, Proposals } from '../typechain';
 
 // This function needs to be declared this way, otherwise it's not understood by test runner.
 // eslint-disable-next-line func-names
@@ -10,6 +10,10 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   if (['hardhat', 'localhost'].includes(hre.network.name)) {
     // Test networks setup
     // TODO automate the flow for testnet deployment - OCT-364
+    const { TestFoundation } = await hre.ethers.getNamedSigners();
+    const epochs: Epochs = await hre.ethers.getContract(EPOCHS);
+    const proposals: Proposals = await hre.ethers.getContract(PROPOSALS);
+    await proposals.connect(TestFoundation).setEpochs(epochs.address);
   } else {
     // Live networks setup
     // Renounce deployer role
