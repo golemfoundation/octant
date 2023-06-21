436550142ff267ea1207faf5228b503a9fbe70af
Michał Kluczek <michal@wildland.io>
2023-06-21 19:16:04 +0200

    OCT-553: Change Epoch's "id" field to Bytes and add new "epoch" Int field

--- subgraph/src/epochs.ts
+++ subgraph/src/epochs.ts
@@ -1,4 +1,4 @@
-import { Address, ethereum, log } from '@graphprotocol/graph-ts';
+import { Address, ethereum, log, Bytes } from '@graphprotocol/graph-ts';
 
 import { Epochs } from '../generated/Epochs/Epochs';
 import { Epoch } from '../generated/schema';
@@ -6,7 +6,7 @@ import { Epoch } from '../generated/schema';
 export function handleBlock(block: ethereum.Block): void {
   // TODO pass the address from config OCT-467
   const epochsContract = Epochs.bind(
-    Address.fromString('0x456F0802243A14C3b5F55B943A57acf64Ac82531'),
+    Address.fromString('0x93f098E71ea4CbbbA137A03C4CDa0517DfA112D8'),
   );
   const currentEpoch = epochsContract.try_getCurrentEpoch();
   if (currentEpoch.reverted) {
@@ -14,19 +14,21 @@ export function handleBlock(block: ethereum.Block): void {
     return;
   }
 
-  let epoch = Epoch.load(currentEpoch.value.toString());
+  const epochNo = currentEpoch.value.toI32()
+  let epoch = Epoch.load(Bytes.fromI32(epochNo));
   if (epoch == null) {
-    epoch = new Epoch(currentEpoch.value.toString());
+    epoch = new Epoch(Bytes.fromI32(epochNo));
 
     const decisionWindow = epochsContract.getDecisionWindow();
     const duration = epochsContract.getEpochDuration();
     const epochEnd = epochsContract.getCurrentEpochEnd();
     const epochStart = epochEnd.minus(duration);
 
-    epoch.fromTs = epochStart.toI32();
-    epoch.toTs = epochEnd.toI32();
-    epoch.duration = duration.toI32();
-    epoch.decisionWindow = decisionWindow.toI32();
+    epoch.epoch = epochNo;
+    epoch.fromTs = epochStart;
+    epoch.toTs = epochEnd;
+    epoch.duration = duration;
+    epoch.decisionWindow = decisionWindow;
 
     epoch.save();
   }
