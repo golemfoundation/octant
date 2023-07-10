4783a92a9236e470fed588ae166e6b6cdb80232d
Michał Kluczek <michal@wildland.io>
2023-07-10 02:01:34 +0200

    OCT-496: Use dedicated webserver instead of vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -52,7 +52,7 @@ export default defineConfig(({ mode }) => {
   }
 
   return {
-    base: '/governance/octant/',
+    base: (isProduction ? '/' : '/governance/octant/'),
     css: {
       modules: {
         generateScopedName: localIdentName,
