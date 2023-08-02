46753a44591cbefae004900e8ba0a8bdfd5b9cd5
Pawel Peregud <paulperegud@protonmail.com>
2023-08-02 12:50:02 +0200

    chore: add `yarn status` - prints status of localhost contracts

--- contracts-v1/tasks/increase-time.ts
+++ contracts-v1/tasks/increase-time.ts
@@ -1,18 +1,16 @@
 import { task } from 'hardhat/config';
 import { int } from 'hardhat/internal/core/params/argumentTypes';
 
-import { EPOCH_DURATION } from '../env';
-
 /* eslint no-console: 0 */
 
 task('increase-time', 'Increase next block time')
-  .addParam('time', 'time in sec', EPOCH_DURATION, int)
+  .addParam('time', 'time in sec', 300, int)
   .setAction(async (taskArgs, { network }) => {
     if (network.name !== 'localhost') {
       console.log('This script will work only with localhost network');
       return;
     }
-
+    console.log(`Forwarding chain time ${taskArgs.time}s`);
     await network.provider.send('evm_increaseTime', [taskArgs.time]);
     await network.provider.send('evm_mine');
   });
