acc623e46e840b70253c13cece99ad4531954d88
Łukasz Kujawski <lukas.kujawski@gmail.com>
2023-08-10 08:38:35 +0000

    OCT-786: current epoch num is not dependent on tz

--- backend/tests/test_epochs.py
+++ backend/tests/test_epochs.py
@@ -30,21 +30,21 @@ def test_get_current_epoch_when_is_after_epoch_1_end():
     assert get_current_epoch() == 2
 
 
-@freeze_time("2023-08-01 01:59:59", tz_offset=+2)
+@freeze_time("2023-07-31 23:59:59", tz_offset=+2)
 def test_get_current_epoch_when_is_before_epoch_0_end_diff_tz():
     assert get_current_epoch() == 0
 
 
-@freeze_time("2023-08-01 02:00:00", tz_offset=+2)
+@freeze_time("2023-08-01 00:00:00", tz_offset=+2)
 def test_get_current_epoch_when_is_after_epoch_0_end_and_before_epoch_1_end_diff_tz():
     assert get_current_epoch() == 1
 
 
-@freeze_time("2023-11-01 01:59:59", tz_offset=+2)
+@freeze_time("2023-10-31 23:59:59", tz_offset=+2)
 def test_get_current_epoch_when_is_before_epoch_1_end_diff_tz():
     assert get_current_epoch() == 1
 
 
-@freeze_time("2023-11-01 02:00:00", tz_offset=+2)
+@freeze_time("2023-11-01 00:00:00", tz_offset=+2)
 def test_get_current_epoch_when_is_after_epoch_1_end_diff_tz():
     assert get_current_epoch() == 2
