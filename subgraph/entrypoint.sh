ddcb33a7ff2c6be3db45c962f2f8410b5177610b
Pawel Peregud <paulperegud@protonmail.com>
2023-08-25 14:20:31 +0200

    chore: remove duplicated code

--- subgraph/entrypoint.sh
+++ subgraph/entrypoint.sh
@@ -2,31 +2,20 @@
 # Docker entrypoint
 set -e
 
-NETWORK=${NETWORK:-"sepolia"}
+export NETWORK=${NETWORK:-"sepolia"}
 SUBGRAPH_URL=${SUBGRAPH_URL:-"http://127.0.0.1:8020"}
 IPFS_URL=${IPFS_URL:-"http://127.0.0.1:5001"}
 LABEL=${VERSION_TAG:-"VM_$HOSTNAME"}
 NODE_NAME=${NODE_NAME:-"octant"}
-NETWORK_FILE=${NETWORK_FILE:-"./networks.json"}
-export GNT_CONTRACT_ADDRESS=$(cat $NETWORK_FILE | jq -r ".${NETWORK}.GNT.address")
-export GLM_CONTRACT_ADDRESS=$(cat $NETWORK_FILE | jq -r ".${NETWORK}.GLM.address")
-export EPOCHS_CONTRACT_ADDRESS=$(cat $NETWORK_FILE | jq -r ".${NETWORK}.Epochs.address")
+export NETWORK_FILE=${NETWORK_FILE:-"./networks.json"}
 
 echo "Node name:    $NODE_NAME"
 echo "Network:      $NETWORK"
 echo "Label:        $LABEL"
 echo "Subgraph URL: $SUBGRAPH_URL"
 echo "IPFS URL:     $IPFS_URL"
-echo "GNT addr:     $GNT_CONTRACT_ADDRESS"
-echo "GLM addr:     $GLM_CONTRACT_ADDRESS"
-echo "EPOCHS addr:  $EPOCHS_CONTRACT_ADDRESS"
-echo
-echo "Replace hardcoded contract addresses"
-for i in src/*template*; do
-    echo "Replacing in $i"
-    F=$(echo $i | sed 's/template\.//')
-    envsubst < $i > $F
-done
+
+./configure-network.sh
 
 echo "Code generation"
 npx graph codegen
