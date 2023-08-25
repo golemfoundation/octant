ebbbe0d07b3ef1f8b07adf43540a48f2578726eb
Pawel Peregud <paulperegud@protonmail.com>
2023-08-25 13:41:46 +0200

    chore: don't keep autogenerated code in the repo

--- subgraph/tests/utils.ts
+++ subgraph/tests/utils.ts
@@ -4,8 +4,8 @@ import { newMockEvent, createMockedFunction } from 'matchstick-as';
 import { Locked as LockedEvent, Unlocked as UnlockedEvent } from '../generated/Deposits/Deposits';
 import { Withdrawn } from '../generated/Vault/Vault';
 
-export const GLM_ADDRESS = Address.fromString('0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512');
-export const GNT_ADDRESS = Address.fromString('0x5FbDB2315678afecb367f032d93F642f64180aa3');
+export const GLM_ADDRESS = Address.fromString('0x7DD9c5Cba05E151C895FDe1CF355C9A1D5DA6429');
+export const GNT_ADDRESS = Address.fromString('0xa74476443119A942dE498590Fe1f2454d7D4aC0d');
 
 export function createLockedEvent(
   depositBefore: BigInt,
