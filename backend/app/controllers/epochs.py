21f7510e848dbb393ecd2311a2a23167f59b5d9c
Paul Peregud <paulperegud@protonmail.ch>
2023-09-04 21:15:22 +0000

    OCT-845 Remove assumption from the backend that Epochs might not be deployed

--- backend/app/controllers/epochs.py
+++ backend/app/controllers/epochs.py
@@ -6,16 +6,4 @@ from app.extensions import epochs
 
 
 def get_current_epoch() -> int:
-    now = datetime.utcnow()
-    epoch_0_end = datetime.utcfromtimestamp(app.config["EPOCH_0_END"])
-    epoch_1_end = datetime.utcfromtimestamp(app.config["EPOCH_1_END"])
-    app.logger.debug(
-        f"now: {now}, epoch 0 end: {epoch_0_end}, epoch 1 end: {epoch_1_end}"
-    )
-
-    if now < epoch_0_end:
-        return 0
-    elif now < epoch_1_end:
-        return 1
-    else:
-        return epochs.get_current_epoch()
+    return epochs.get_current_epoch()
