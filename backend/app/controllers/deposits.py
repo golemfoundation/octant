95de36154ba5cf6ebb7d6f09d28e17944e4d005d
Łukasz Kujawski <lukas.kujawski@gmail.com>
2023-09-06 10:57:55 +0000

    [HOTFIX] Estimated ed without graph

--- backend/app/controllers/deposits.py
+++ backend/app/controllers/deposits.py
@@ -1,18 +1,14 @@
-from flask import current_app as app
-
 from app.controllers import epochs as epochs_controller
 from app.core.deposits import deposits as deposits_core
-from app.infrastructure import graphql
 from app.database import deposits as deposits_db
-from app.settings import config
 
 
 def get_by_user_and_epoch(user_address: str, epoch: int) -> int:
     current_epoch = epochs_controller.get_current_epoch()
 
     if current_epoch == 1 and epoch == 1:
-        details = graphql.epochs.get_epoch_by_number(epoch)
-        start, end = int(details["fromTs"]), int(details["toTs"])
+        # TODO fetch these timestamps from subgraph during implementation of OCT-859
+        start, end = 1691510400, 1697731200
         return deposits_core.get_estimated_effective_deposit(start, end, user_address)
 
     deposit = deposits_db.get_by_user_address_and_epoch(user_address, epoch)
