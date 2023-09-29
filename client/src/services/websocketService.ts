46cb0a7398da789290f8a8ce6e4941bd12802660
Andrzej Ziółek <a.j.ziolek@gmail.com>
2023-09-29 13:25:23 +0000

    [FIX] client websocket policy adjusted to avoid errors

--- client/src/services/websocketService.ts
+++ client/src/services/websocketService.ts
@@ -2,4 +2,4 @@ import { io } from 'socket.io-client';
 
 import env from 'env';
 
-export default io(env.websocketEndpoint, { withCredentials: true });
+export default io(env.websocketEndpoint, { transports : ['websocket'] });
