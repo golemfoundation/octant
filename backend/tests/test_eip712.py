073706ea3aee6eb7b197d756b92bd80772a798cf
Piotr Żelazko <pzelazko@wildland.io>
2023-10-03 16:04:51 +0200

    test: extract test for checking nonces

--- backend/tests/test_eip712.py
+++ backend/tests/test_eip712.py
@@ -25,7 +25,8 @@ def test_sign_and_recover_address_from_allocations(user_accounts):
                 "proposalAddress": "0x2345678901234567890123456789012345678904",
                 "amount": 250,
             },
-        ]
+        ],
+        "nonce": 0,
     }
 
     eip712_data = build_allocations_eip712_data(payload)
@@ -48,7 +49,8 @@ def test_fails_when_data_to_recover_has_changed(user_accounts):
                 "proposalAddress": "0x2345678901234567890123456789012345678904",
                 "amount": 250,
             },
-        ]
+        ],
+        "nonce": 0,
     }
     eip712_data = build_allocations_eip712_data(payload)
     signature = sign(account, eip712_data)
@@ -59,8 +61,34 @@ def test_fails_when_data_to_recover_has_changed(user_accounts):
                 "proposalAddress": "0x0000000000000000000000000000000000000000",
                 "amount": 150000,
             },
-        ]
+        ],
+        "nonce": 0,
     }
+    eip712_data = build_allocations_eip712_data(changed_payload)
+    address = recover_address(eip712_data, signature)
+
+    assert address != account.address
+
+
+def test_fails_when_nonce_has_changed(user_accounts):
+    account = user_accounts[0]
+    payload = {
+        "allocations": [
+            {
+                "proposalAddress": "0x1234567890123456789012345678901234567893",
+                "amount": 150,
+            },
+            {
+                "proposalAddress": "0x2345678901234567890123456789012345678904",
+                "amount": 250,
+            },
+        ],
+        "nonce": 0,
+    }
+    eip712_data = build_allocations_eip712_data(payload)
+    signature = sign(account, eip712_data)
+
+    changed_payload = {"allocations": payload["allocations"], "nonce": 1}
     eip712_data = build_allocations_eip712_data(changed_payload)
     address = recover_address(eip712_data, signature)
 
