a3d05d39569c6427976ac178c454b79fd052af45
Łukasz Kujawski <leon@wildland.io>
2023-10-05 10:03:44 +0200

    feat: enable/disable patron mode endpoint - WIP

--- backend/app/crypto/eth_sign/terms_and_conditions_consent.py
+++ backend/app/crypto/eth_sign/terms_and_conditions_consent.py
@@ -1,10 +1,4 @@
-from eth_account.account import Account
-from eth_account.messages import encode_defunct
-from eth_keys.exceptions import BadSignature
-from web3.exceptions import ContractLogicError
-
-from app.crypto.account import is_contract
-from app.crypto.eip1271 import is_valid_signature
+from app.crypto.eth_sign.signature import verify_signed_message
 
 
 def build_consent_message(user_address: str) -> str:
@@ -21,27 +15,7 @@ def build_consent_message(user_address: str) -> str:
     )
 
 
-def verify_signed_message(user_address: str, signature: str) -> bool:
+def verify(user_address: str, signature: str) -> bool:
     msg_text = build_consent_message(user_address)
 
-    if is_contract(user_address):
-        return _verify_multisig(user_address, msg_text, signature)
-    else:
-        return _verify_eoa(user_address, msg_text, signature)
-
-
-def _verify_multisig(user_address: str, msg_text: str, signature: str) -> bool:
-    try:
-        return is_valid_signature(user_address, msg_text, signature)
-    except ContractLogicError:
-        return False
-
-
-def _verify_eoa(user_address: str, msg_text: str, signature: str) -> bool:
-    encoded_msg = encode_defunct(text=msg_text)
-    try:
-        recovered_address = Account.recover_message(encoded_msg, signature=signature)
-    except BadSignature:
-        return False
-
-    return recovered_address == user_address
+    return verify_signed_message(user_address, msg_text, signature)
