72ef8170d34100e033a03ec663e5f4baddd636e8
Łukasz Kujawski <leon@wildland.io>
2023-10-05 10:03:44 +0200

    feat: enable/disable patron mode endpoint - WIP

--- backend/app/crypto/eth_sign/patron_mode.py
+++ backend/app/crypto/eth_sign/patron_mode.py
@@ -1,3 +1,12 @@
+from app.crypto.eth_sign.signature import verify_signed_message
+
+
 def build_patron_mode_msg(user_address: str, toggle: bool) -> str:
     toggle_msg = "enable" if toggle else "disable"
     return f"Signing this message will {toggle_msg} patron mode for address {user_address}."
+
+
+def verify(user_address: str, toggle: bool, signature: str) -> bool:
+    msg_text = build_patron_mode_msg(user_address, toggle)
+
+    return verify_signed_message(user_address, msg_text, signature)
