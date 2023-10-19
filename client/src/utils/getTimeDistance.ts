252d62a14b90705ff8d63b89047cf32847673589
Jakub Mikołajczyk <jakubmikolajczyk@Jakubs-MacBook-Pro-2.local>
2023-10-19 12:56:44 +0200

    oct-1065 cr fixes

--- client/src/utils/getTimeDistance.ts
+++ client/src/utils/getTimeDistance.ts
@@ -1,10 +1,12 @@
-import { differenceInSeconds } from 'date-fns';
-import formatDistance from 'date-fns/formatDistance';
+import { differenceInSeconds, formatDistance } from 'date-fns';
+import i18n from 'i18n';
 
 const getTimeDistance = (fromTimestamp: number, toTimestamp: number): string => {
   const differenceSeconds = Math.abs(differenceInSeconds(fromTimestamp, toTimestamp));
 
-  if (differenceSeconds < 60) {return 'less than 1m';}
+  if (differenceSeconds < 60) {
+    return i18n.t('common.lessThan1m');
+  }
   return formatDistance(new Date(fromTimestamp), new Date(toTimestamp));
 };
 
