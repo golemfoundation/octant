c73ae164fc9fc90970ffc896f99df24c5a80c898
Jakub Mikołajczyk <jakubmikolajczyk@Jakubs-MacBook-Pro-2.local>
2023-10-19 12:44:56 +0200

    oct-1065 navbar countdown one line

--- client/src/utils/getTimeDistance.ts
+++ client/src/utils/getTimeDistance.ts
@@ -1,6 +1,11 @@
+import { differenceInSeconds } from 'date-fns';
 import formatDistance from 'date-fns/formatDistance';
 
-const getTimeDistance = (fromTimestamp: number, toTimestamp: number): string =>
-  formatDistance(new Date(fromTimestamp), new Date(toTimestamp));
+const getTimeDistance = (fromTimestamp: number, toTimestamp: number): string => {
+  const differenceSeconds = Math.abs(differenceInSeconds(fromTimestamp, toTimestamp));
+
+  if (differenceSeconds < 60) {return 'less than 1m';}
+  return formatDistance(new Date(fromTimestamp), new Date(toTimestamp));
+};
 
 export default getTimeDistance;
