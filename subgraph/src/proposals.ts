7eaa4239bd2c9521d06d59a56d06c7d323f9afe4
AV <artem.bv@pm.me>
2023-11-01 21:33:52 +0300

    feat: rename entity to EpochProjectsInfo

--- subgraph/src/proposals.ts
+++ subgraph/src/proposals.ts
@@ -1,13 +1,13 @@
 import { Bytes } from '@graphprotocol/graph-ts';
 
 import { SetProposalAddressesCall } from '../generated/Proposals/Proposals';
-import { EpochProjects } from '../generated/schema';
+import { EpochProjectsInfo } from '../generated/schema';
 
 export function handleSetProposalAddresses(call: SetProposalAddressesCall): void {
   const epochAsId = Bytes.fromByteArray(Bytes.fromBigInt(call.inputs._epoch));
-  let epochProjects: EpochProjects | null = EpochProjects.load(epochAsId);
+  let epochProjects: EpochProjectsInfo | null = EpochProjectsInfo.load(epochAsId);
   if (epochProjects === null) {
-    epochProjects = new EpochProjects(epochAsId);
+    epochProjects = new EpochProjectsInfo(epochAsId);
     epochProjects.epoch = call.inputs._epoch.toI32();
     // eslint-disable-next-line no-undef
     epochProjects.projectsAddresses = changetype<Bytes[]>(call.inputs._proposalAddresses);
--- subgraph/tests/proposals.test.ts
+++ subgraph/tests/proposals.test.ts
@@ -3,7 +3,7 @@ import { afterAll, beforeAll, describe, test, assert, clearStore } from 'matchst
 
 import { createSetProposalAddressesCall } from './utils';
 
-import { EpochProjects } from '../generated/schema';
+import { EpochProjectsInfo } from '../generated/schema';
 import { handleSetProposalAddresses } from '../src/proposals';
 
 // Tests structure (matchstick-as >=0.5.0)
@@ -58,7 +58,7 @@ const epoch1Addresses = [
 
 describe('handleSetProposalAddresses', () => {
   beforeAll(() => {
-    const epochProjects = new EpochProjects(Bytes.fromI32(0));
+    const epochProjects = new EpochProjectsInfo(Bytes.fromI32(0));
     epochProjects.epoch = 0;
     // eslint-disable-next-line no-undef
     epochProjects.projectsAddresses = changetype<Bytes[]>(epoch0Addresses);
@@ -66,11 +66,11 @@ describe('handleSetProposalAddresses', () => {
   });
 
   afterAll(() => {
-    // store.remove('EpochProjects', Bytes.fromI32(1).toHexString())
+    // store.remove('EpochProjectsInfo', Bytes.fromI32(1).toHexString())
     clearStore();
   });
 
-  test('Should create a new EpochProjects entity', () => {
+  test('Should create a new EpochProjectsInfo entity', () => {
     // const proposalsAddress = Address.fromString('0x91B904e8B0F9133D0766059065C2A1F6c6CAfA27')
 
     // createMockedFunction(
@@ -87,32 +87,32 @@ describe('handleSetProposalAddresses', () => {
     const setProposalAddressesCall = createSetProposalAddressesCall(1, epoch1Addresses);
     handleSetProposalAddresses(setProposalAddressesCall);
 
-    assert.entityCount('EpochProjects', 2);
-    assert.fieldEquals('EpochProjects', Bytes.fromI32(0).toHexString(), 'epoch', '0');
-    assert.fieldEquals('EpochProjects', Bytes.fromI32(1).toHexString(), 'epoch', '1');
+    assert.entityCount('EpochProjectsInfo', 2);
+    assert.fieldEquals('EpochProjectsInfo', Bytes.fromI32(0).toHexString(), 'epoch', '0');
+    assert.fieldEquals('EpochProjectsInfo', Bytes.fromI32(1).toHexString(), 'epoch', '1');
     assert.fieldEquals(
-      'EpochProjects',
+      'EpochProjectsInfo',
       Bytes.fromI32(0).toHexString(),
       'projectsAddresses',
       '[0x00de4b13153673bcae2616b67bf822500d325fc3, 0x1c01595f9534e33d411035ae99a4317faec4f6fe, 0x02cb3c150bedca124d0ae8cccb72fefbe705c953, 0x6e8873085530406995170da467010565968c7c62, 0x7dac9fc15c1db4379d75a6e3f330ae849dffce18, 0xab6d6a37c5110d1377832c451c33e4fa16a9ba05, 0xcc7d34c76a9d08aa0109f7bae35f29c1ce35355a, 0xd165df4296c85e780509fa1eace0150d945d49fd, 0xde21f729137c5af1b01d73af1dc21effa2b8a0d6, 0xf6cbdd6ea6ec3c4359e33de0ac823701cc56c6c4]',
     );
 
     assert.fieldEquals(
-      'EpochProjects',
+      'EpochProjectsInfo',
       Bytes.fromI32(1).toHexString(),
       'projectsAddresses',
       '[0x1c01595f9534e33d411035ae99a4317faec4f6fe, 0x02cb3c150bedca124d0ae8cccb72fefbe705c953, 0x6e8873085530406995170da467010565968c7c62, 0x7dac9fc15c1db4379d75a6e3f330ae849dffce18, 0xab6d6a37c5110d1377832c451c33e4fa16a9ba05, 0xcc7d34c76a9d08aa0109f7bae35f29c1ce35355a, 0xd165df4296c85e780509fa1eace0150d945d49fd, 0xde21f729137c5af1b01d73af1dc21effa2b8a0d6, 0xf6cbdd6ea6ec3c4359e33de0ac823701cc56c6c4, 0x0b7246ef74ca7b37fdc3d15be4f0b49876622f95, 0x2dcdf80f439843d7e0ad1fef9e7a439b7917eac9, 0x4a9a27d614a74ee5524909ca27bdbcbb7ed3b315, 0x4adc8cc149a03f44386bee80bab36f9e8022b195, 0x8c89a6bf53ccf63e7b4465cc1b1330723b4bdcb7, 0x20a1b17087482de88fac6d7b5ae23a7175fd1395, 0x78e084445c3f1006617e1f36794dd2261ece4ae3, 0x149d46ec060e75ae188876adb6b24024637003c7, 0x15c941a44a343b8c46a28f2bb9afc7a54e255a4f, 0x3455fbb4d34c6b47999b66c83aa7bd8fddade638, 0x9531c059098e3d194ff87febb587ab07b30b1306, 0xcf3efce169acec1b281c05e863f78accf62bd944, 0xd1b8db70ded72db850713b2ce7e1a4ffafad95d1, 0xf0652a820dd39ec956659e0018da022132f2f40a, 0xffbd35255008f86322051f2313d4b343540e0e00]',
     );
   });
 
-  test('Should create and update a new EpochProjects entity', () => {
+  test('Should create and update a new EpochProjectsInfo entity', () => {
     const setProposalAddressesCall = createSetProposalAddressesCall(1, epoch1Addresses);
     handleSetProposalAddresses(setProposalAddressesCall);
 
-    assert.entityCount('EpochProjects', 2);
-    assert.fieldEquals('EpochProjects', Bytes.fromI32(1).toHexString(), 'epoch', '1');
+    assert.entityCount('EpochProjectsInfo', 2);
+    assert.fieldEquals('EpochProjectsInfo', Bytes.fromI32(1).toHexString(), 'epoch', '1');
     assert.fieldEquals(
-      'EpochProjects',
+      'EpochProjectsInfo',
       Bytes.fromI32(1).toHexString(),
       'projectsAddresses',
       '[0x1c01595f9534e33d411035ae99a4317faec4f6fe, 0x02cb3c150bedca124d0ae8cccb72fefbe705c953, 0x6e8873085530406995170da467010565968c7c62, 0x7dac9fc15c1db4379d75a6e3f330ae849dffce18, 0xab6d6a37c5110d1377832c451c33e4fa16a9ba05, 0xcc7d34c76a9d08aa0109f7bae35f29c1ce35355a, 0xd165df4296c85e780509fa1eace0150d945d49fd, 0xde21f729137c5af1b01d73af1dc21effa2b8a0d6, 0xf6cbdd6ea6ec3c4359e33de0ac823701cc56c6c4, 0x0b7246ef74ca7b37fdc3d15be4f0b49876622f95, 0x2dcdf80f439843d7e0ad1fef9e7a439b7917eac9, 0x4a9a27d614a74ee5524909ca27bdbcbb7ed3b315, 0x4adc8cc149a03f44386bee80bab36f9e8022b195, 0x8c89a6bf53ccf63e7b4465cc1b1330723b4bdcb7, 0x20a1b17087482de88fac6d7b5ae23a7175fd1395, 0x78e084445c3f1006617e1f36794dd2261ece4ae3, 0x149d46ec060e75ae188876adb6b24024637003c7, 0x15c941a44a343b8c46a28f2bb9afc7a54e255a4f, 0x3455fbb4d34c6b47999b66c83aa7bd8fddade638, 0x9531c059098e3d194ff87febb587ab07b30b1306, 0xcf3efce169acec1b281c05e863f78accf62bd944, 0xd1b8db70ded72db850713b2ce7e1a4ffafad95d1, 0xf0652a820dd39ec956659e0018da022132f2f40a, 0xffbd35255008f86322051f2313d4b343540e0e00]',
@@ -121,10 +121,10 @@ describe('handleSetProposalAddresses', () => {
     const setProposalAddressesCallUpdate = createSetProposalAddressesCall(1, epoch0Addresses);
     handleSetProposalAddresses(setProposalAddressesCallUpdate);
 
-    assert.entityCount('EpochProjects', 2);
-    assert.fieldEquals('EpochProjects', Bytes.fromI32(1).toHexString(), 'epoch', '1');
+    assert.entityCount('EpochProjectsInfo', 2);
+    assert.fieldEquals('EpochProjectsInfo', Bytes.fromI32(1).toHexString(), 'epoch', '1');
     assert.fieldEquals(
-      'EpochProjects',
+      'EpochProjectsInfo',
       Bytes.fromI32(1).toHexString(),
       'projectsAddresses',
       '[0x00de4b13153673bcae2616b67bf822500d325fc3, 0x1c01595f9534e33d411035ae99a4317faec4f6fe, 0x02cb3c150bedca124d0ae8cccb72fefbe705c953, 0x6e8873085530406995170da467010565968c7c62, 0x7dac9fc15c1db4379d75a6e3f330ae849dffce18, 0xab6d6a37c5110d1377832c451c33e4fa16a9ba05, 0xcc7d34c76a9d08aa0109f7bae35f29c1ce35355a, 0xd165df4296c85e780509fa1eace0150d945d49fd, 0xde21f729137c5af1b01d73af1dc21effa2b8a0d6, 0xf6cbdd6ea6ec3c4359e33de0ac823701cc56c6c4]',
