ad76cec52713475cb7adcb4a359eb0732a29f24d
Jakub Mikołajczyk <jakub@wildland.io>
2023-12-13 07:26:04 +0000

    OCT-1165: CY: create a job for E2+

--- contracts-v1/entrypoint.sh
+++ contracts-v1/entrypoint.sh
@@ -19,3 +19,9 @@ fi
 if [[ $2 ]]; then
 	cp deployments/clientEnv $2
 fi
+
+if [[ "${FORWARD_FIRST_EPOCH-:}" == "true" ]]; then
+	echo "Forwarding first epoch ..." 
+  r=$(curl -s -X POST "${LOCAL_RPC_URL}" -H "Content-Type: application/json" --data "{\"jsonrpc\":\"2.0\",\"method\":\"evm_increaseTime\",\"params\":[${EPOCH_DURATION}],\"id\":1}")
+	echo "Forwarded time by ${EPOCH_DURATION}: ${r}"
+fi
