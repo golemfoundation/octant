efc0dd6f27beb0794824c2748e93c53c15e43202
Piotr Żelazko <pzelazko@golem.foundation>
2023-12-21 13:17:38 +0100

    fix: move networks.json file generation to main subgraph entrypoint

--- localenv/subgraph-deployer/entrypoint.sh
+++ localenv/subgraph-deployer/entrypoint.sh
@@ -9,16 +9,10 @@ wait_for_contracts(){
       -s -X GET "${CONTRACTS_DEPLOYER_URL}" 
 }
 
-generate_network_file(){
-  envsubst <./networks.template.json >$NETWORK_FILE
-}
-
 if [ ${LOCAL_CONTRACTS:-} == "true" ] ; then
   echo Waiting for contracts deployment ...
   wait_for_contracts > localhost-contracts
   set -a && source localhost-contracts && set +a
 fi
-
-generate_network_file
 
 ./entrypoint.sh
--- subgraph/configure-network.sh
+++ subgraph/configure-network.sh
@@ -3,14 +3,21 @@
 set -ue
 
 NETWORK=${NETWORK:-"localhost"}
+
+NETWORK_TEMPLATE_FILE=${NETWORK_TEMPLATE_FILE:-"./networks.template.json"}
 NETWORK_FILE=${NETWORK_FILE:-"./networks.json"}
 
 ARTIFACTS_DIR="./generated"
 mkdir -p "${ARTIFACTS_DIR}"
 
-export GLM_CONTRACT_ADDRESS=$(cat $NETWORK_FILE | jq -r ".${NETWORK}.GLM.address")
-export EPOCHS_CONTRACT_ADDRESS=$(cat $NETWORK_FILE | jq -r ".${NETWORK}.Epochs.address")
-export PROPOSALS_CONTRACT_ADDRESS=$(cat $NETWORK_FILE | jq -r ".${NETWORK}.Proposals.address")
+echo "Generating network file ${NETWORK_FILE} from ${NETWORK_TEMPLATE_FILE}" 
+echo 
+
+envsubst <$NETWORK_TEMPLATE_FILE >$NETWORK_FILE
+
+export GLM_CONTRACT_ADDRESS=$(jq -r ".${NETWORK}.GLM.address" <$NETWORK_FILE)
+export EPOCHS_CONTRACT_ADDRESS=$(jq -r ".${NETWORK}.Epochs.address" <$NETWORK_FILE)
+export PROPOSALS_CONTRACT_ADDRESS=$(jq -r ".${NETWORK}.Proposals.address" <$NETWORK_FILE)
 
 echo "Network:         $NETWORK"
 echo "GLM addr:        $GLM_CONTRACT_ADDRESS"
