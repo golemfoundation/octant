5ec714a82f44fd81dc91c9da65038609b8b7ab0b
Łukasz Kujawski <leon@wildland.io>
2023-12-22 08:04:20 +0100

    test: adjust test scenario to epoch 2

--- backend/tests/deposits/test_weighted_average.py
+++ backend/tests/deposits/test_weighted_average.py
@@ -88,7 +88,7 @@ def epoch_details():
                 },
             ],
             {
-                "effective_deposit": "999_500000000_000000000",
+                "effective_deposit": "500_000000000_000000000",
                 "epoch_end_deposit": "500_000000000_000000000",
             },
         ),
@@ -169,7 +169,7 @@ def epoch_details():
                 },
             ],
             {
-                "effective_deposit": "119_433886492_855790068",
+                "effective_deposit": "104_433886492_855790068",
                 "epoch_end_deposit": "320_448372019_117283735",
             },
         ),
@@ -217,7 +217,7 @@ def epoch_details():
 def test_update_user_deposits(mocker, events, expected):
     """Epochs start == 1000, epoch end == 2000"""
 
-    epoch = 1
+    epoch = 2
 
     mock_graphql(
         mocker, epochs_events=[create_epoch_event(epoch=epoch)], deposit_events=events
