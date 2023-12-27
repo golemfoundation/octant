fb924a2c6e75c628b3f629aa788c62353985363e
Łukasz Kujawski <leon@wildland.io>
2023-12-27 21:28:08 +0100

    test: fix module tests

--- backend/tests/v2/modules/user/budgets/test_estimate_budget.py
+++ backend/tests/v2/modules/user/budgets/test_estimate_budget.py
@@ -1,21 +1,33 @@
 import pytest
 
+from app.utils.time import days_to_sec
 from app.v2.modules.user.budgets.service.service import estimate_budget
 from tests.helpers.context import get_context
 
 
 @pytest.mark.parametrize(
-    "lock_duration,amount,expected",
+    "days,amount,expected",
     [
-        (400, 500_000000000_000000000, 508956_310956346),
-        (600, 500_000000000_000000000, 763434_466434519),
-        (1750, 500_000000000_000000000, 1781347_088347212),
-        (2750, 500_000000000_000000000, 2799259_710259905),
+        (0, 0, 0),
+        (15, 90, 0),
+        (15, 1000_000000000_000000000, 0),
+        (15, 300000_000000000_000000000, 0),
+        (70, 90_000000000_000000000, 0),
+        (70, 1000_000000000_000000000, 981783_007246039),
+        (70, 300000_000000000_000000000, 294534902_173811818),
+        (150, 90_000000000_000000000, 0),
+        (150, 1000_000000000_000000000, 981783_007246039),
+        (150, 300000_000000000_000000000, 294534902_173811818),
+        (252, 90_000000000_000000000, 0),
+        (252, 1000_000000000_000000000, 3017608_251071425),
+        (252, 300000_000000000_000000000, 905282475_321428004),
+        (365250, 300000_000000000_000000000, 1239_195987032_113245119),
     ],
 )
-def test_estimate_budget(lock_duration, amount, expected):
-    current_context = get_context(2, remaining_sec=500)
-    future_context = get_context(3, remaining_sec=1000)
+def test_estimate_budget(days, amount, expected):
+    current_context = get_context(1, start=10000000, duration=6220800, remaining_sec=6000000)
+    future_context = get_context(2, start=16220800, duration=7776000, remaining_sec=7776000)
+    lock_duration = days_to_sec(days)
 
     result = estimate_budget(current_context, future_context, lock_duration, amount)
 
