f0467210d4c8cebcd3be820109c42c579a4e8cba
Jakub Mikołajczyk <jakub@wildland.io>
2024-01-29 09:18:49 +0000

    OCT-1219: Implement allocation reminder for users with unallocated funds close to end of allocation

--- client/src/components/Earn/EarnTipTiles/EarnTipTiles.tsx
+++ client/src/components/Earn/EarnTipTiles/EarnTipTiles.tsx
@@ -1,11 +1,16 @@
+import { differenceInCalendarDays } from 'date-fns';
 import React, { Fragment, ReactElement } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useAccount } from 'wagmi';
 
 import TipTile from 'components/shared/TipTile';
+import useEpochAndAllocationTimestamps from 'hooks/helpers/useEpochAndAllocationTimestamps';
 import useMediaQuery from 'hooks/helpers/useMediaQuery';
 import useCurrentEpoch from 'hooks/queries/useCurrentEpoch';
 import useDepositValue from 'hooks/queries/useDepositValue';
+import useIndividualReward from 'hooks/queries/useIndividualReward';
+import useIsDecisionWindowOpen from 'hooks/queries/useIsDecisionWindowOpen';
+import useUserAllocations from 'hooks/queries/useUserAllocations';
 import useWithdrawals from 'hooks/queries/useWithdrawals';
 import useTipsStore from 'store/tips/store';
 import getIsPreLaunch from 'utils/getIsPreLaunch';
@@ -20,6 +25,10 @@ const EarnTipTiles = (): ReactElement => {
   const { data: currentEpoch } = useCurrentEpoch();
   const isPreLaunch = getIsPreLaunch(currentEpoch);
   const { data: depositsValue, isFetching: isFetchingDepositsValue } = useDepositValue();
+  const { data: individualReward } = useIndividualReward();
+  const { data: userAllocations } = useUserAllocations();
+  const { timeCurrentAllocationEnd } = useEpochAndAllocationTimestamps();
+  const { data: isDecisionWindowOpen } = useIsDecisionWindowOpen();
 
   const {
     wasWithdrawAlreadyClosed,
@@ -28,10 +37,14 @@ const EarnTipTiles = (): ReactElement => {
     setWasConnectWalletAlreadyClosed,
     wasLockGLMAlreadyClosed,
     setWasLockGLMAlreadyClosed,
+    wasAllocateRewardsAlreadyClosed,
+    setWasAllocateRewardsAlreadyClosed,
   } = useTipsStore(state => ({
+    setWasAllocateRewardsAlreadyClosed: state.setWasAllocateRewardsAlreadyClosed,
     setWasConnectWalletAlreadyClosed: state.setWasConnectWalletAlreadyClosed,
     setWasLockGLMAlreadyClosed: state.setWasLockGLMAlreadyClosed,
     setWasWithdrawAlreadyClosed: state.setWasWithdrawAlreadyClosed,
+    wasAllocateRewardsAlreadyClosed: state.data.wasAllocateRewardsAlreadyClosed,
     wasConnectWalletAlreadyClosed: state.data.wasConnectWalletAlreadyClosed,
     wasLockGLMAlreadyClosed: state.data.wasLockGLMAlreadyClosed,
     wasWithdrawAlreadyClosed: state.data.wasWithdrawAlreadyClosed,
@@ -49,6 +62,14 @@ const EarnTipTiles = (): ReactElement => {
     !!withdrawals &&
     !withdrawals.sums.available.isZero() &&
     !wasWithdrawAlreadyClosed;
+
+  const isAllocateRewardsTipVisible =
+    (!wasAllocateRewardsAlreadyClosed &&
+      isDecisionWindowOpen &&
+      !individualReward?.isZero() &&
+      !userAllocations?.hasUserAlreadyDoneAllocation &&
+      differenceInCalendarDays(new Date(), timeCurrentAllocationEnd!) <= 2) ??
+    false;
 
   return (
     <Fragment>
@@ -76,6 +97,16 @@ const EarnTipTiles = (): ReactElement => {
         onClose={() => setWasWithdrawAlreadyClosed(true)}
         text={t('withdrawEth.text')}
         title={t('withdrawEth.title')}
+      />
+      <TipTile
+        image="images/funds_swept.webp"
+        infoLabel={i18n.t('common.octantTips')}
+        isOpen={isAllocateRewardsTipVisible}
+        onClose={() => setWasAllocateRewardsAlreadyClosed(true)}
+        text={
+          isDesktop ? t('allocateYourRewards.textDesktop') : t('allocateYourRewards.textMobile')
+        }
+        title={t('allocateYourRewards.title')}
       />
     </Fragment>
   );
