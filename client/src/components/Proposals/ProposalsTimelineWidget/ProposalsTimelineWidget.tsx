f0a54e894a2416631dfc29b9c9240deb82697aa6
Jakub Mikołajczyk <jakub@wildland.io>
2024-01-29 09:20:22 +0000

    OCT-1180: Implement Project view timeline widget

---
+++ client/src/components/Proposals/ProposalsTimelineWidget/ProposalsTimelineWidget.tsx
@@ -0,0 +1,100 @@
+import { isAfter, isSameDay, isWithinInterval } from 'date-fns';
+import { motion, useMotionValue } from 'framer-motion';
+import React, { ReactElement, useLayoutEffect, useRef } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import ProposalsTimelineWidgetItem from 'components/Proposals/ProposalsTimelineWidgetItem';
+
+import styles from './ProposalsTimelineWidget.module.scss';
+import { Milestone } from './types';
+
+const ProposalsTimelineWidget = (): ReactElement => {
+  const { t } = useTranslation('translation', {
+    keyPrefix: 'views.proposals.proposalsTimelineWidget',
+  });
+  const constraintsRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+
+  // LOOK AT ME
+  // Each milestone must have a date in the appropriate Europe/Warsaw time zone!
+  const milestones: Milestone[] = [
+    {
+      from: new Date('2023-11-13 GMT+0100'),
+      id: 'applications-open',
+      label: t('applicationsOpen'),
+      to: new Date('2023-12-20 GMT+0100'),
+    },
+    {
+      from: new Date('2023-12-22 GMT+0100'),
+      id: 'project-updates-close',
+      label: t('projectUpdatesClose'),
+    },
+    {
+      from: new Date('2024-01-01 GMT+0100'),
+      id: 'snapshot-vote',
+      label: t('snapshotVote'),
+      to: new Date('2024-01-05 GMT+0100'),
+    },
+    {
+      from: new Date('2024-01-17 GMT+0100'),
+      id: 'allocation-window',
+      label: t('allocationWindow'),
+      to: new Date('2024-01-31 GMT+0100'),
+    },
+    {
+      from: new Date('2024-01-17 GMT+0100'),
+      id: 'epoch-starts',
+      label: t('epochStarts', { epoch: 'Three' }),
+    },
+  ];
+
+  const milestonesWithIsActive = milestones.reduce(
+    (acc, curr, idx) => {
+      const currentDate = new Date();
+      const isActive =
+        (curr.to
+          ? isWithinInterval(currentDate, {
+              end: curr.to,
+              start: curr.from,
+            })
+          : isSameDay(currentDate, curr.from)) ||
+        (acc.length > 0 && isAfter(curr.from, currentDate) && !acc[idx - 1].isActive);
+
+      acc.push({ ...curr, isActive });
+
+      return acc;
+    },
+    [] as (Milestone & { isActive: boolean })[],
+  );
+
+  useLayoutEffect(() => {
+    const { id } = milestonesWithIsActive.find(({ isActive }) => isActive)!;
+    const el = document.getElementById(id);
+    const elRight = el!.getBoundingClientRect()!.right;
+    const containerRight = constraintsRef.current!.getBoundingClientRect().right;
+    const elOffset = 24;
+
+    const xMotionValue = elRight > containerRight ? -elRight - elOffset + containerRight : 0;
+    x.set(xMotionValue);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <motion.div className={styles.root}>
+      <div ref={constraintsRef} className={styles.constraintsWrapper}>
+        <motion.div
+          className={styles.milestonesWrapper}
+          drag="x"
+          dragConstraints={constraintsRef}
+          style={{ x }}
+        >
+          {milestonesWithIsActive.map(({ id, ...milestone }) => (
+            <ProposalsTimelineWidgetItem key={id} id={id} {...milestone} />
+          ))}
+        </motion.div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default ProposalsTimelineWidget;
