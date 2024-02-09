e8986bc878db9b702d0f741b21f795b7180f7042
Jakub Mikołajczyk <jakubmikolajczyk@Jakubs-MacBook-Pro-2.local>
2024-02-09 09:02:05 +0100

    oct-1119 vite-json-rpc ci/agrocd test 3

--- ci/argocd/application.sh
+++ ci/argocd/application.sh
@@ -65,6 +65,7 @@ elif [[ "$ACTION" == "update" ]]; then
 
 	if [[ "$NETWORK_NAME" == "local" || "$NETWORK_NAME" == "localhost" ]]; then
 		export FRONTEND_RPC_URL=https://$(bash $CI_PROJECT_DIR/ci/argocd/get_rpc_url.sh)
+        export VITE_JSON_RPC_ENDPOINT=https://$(bash $CI_PROJECT_DIR/ci/argocd/get_rpc_url.sh)
 		export BACKEND_RPC_URL=http://anvil:8545
 	else
 		# This will make webclient use default (wagmi) endpoint
