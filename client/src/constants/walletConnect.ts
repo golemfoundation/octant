b55544349220c51cadf23faa39154a6f66f485ca
Andrzej Ziółek <a.j.ziolek@gmail.com>
2024-03-05 15:42:12 +0100

    fix: potential workaround

--- client/src/constants/walletConnect.ts
+++ client/src/constants/walletConnect.ts
@@ -1,6 +1,9 @@
+import { mainnet } from 'wagmi/chains';
+
 import env from 'env';
 
 import networkConfig from './networkConfig';
 
-export const CHAINS = networkConfig.chains;
+// https://github.com/orgs/WalletConnect/discussions/2880#discussioncomment-7237794
+export const CHAINS = [...networkConfig.chains, mainnet];
 export const PROJECT_ID = env.walletConnectProjectId;
