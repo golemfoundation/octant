69dccdf4070e5fe0e3221ee2e341acb703b83ec0
Andrzej Ziółek <a.j.ziolek@gmail.com>
2024-03-05 09:18:31 +0100

    OCT-1453 GLM lock/unlock: changing tab should trigger the validation (#50)

--- client/src/components/Earn/EarnGlmLock/types.ts
+++ client/src/components/Earn/EarnGlmLock/types.ts
@@ -1,10 +1,21 @@
+import { FormikHelpers } from 'formik';
+
 export type CurrentMode = 'lock' | 'unlock';
 
 export type Step = 1 | 2 | 3;
 
 export type FormFields = {
+  currentMode: CurrentMode;
   valueToDeposeOrWithdraw: string;
 };
+
+export type OnReset = ({
+  setFieldValue,
+  newMode,
+}: {
+  newMode: CurrentMode;
+  setFieldValue?: FormikHelpers<FormFields>['setFieldValue'];
+}) => void;
 
 export default interface EarnGlmLockProps {
   currentMode: CurrentMode;
