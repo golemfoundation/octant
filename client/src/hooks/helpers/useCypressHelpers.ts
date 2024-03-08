5ec3c68becd95a215857546a0ba87d192c61d3cb
Andrzej Ziółek <a.j.ziolek@gmail.com>
2024-03-08 14:11:24 +0100

    test: debug

--- client/src/hooks/helpers/useCypressHelpers.ts
+++ client/src/hooks/helpers/useCypressHelpers.ts
@@ -7,8 +7,16 @@ export default function useCypressHelpers(): void {
 
   useEffect(() => {
     /**
-     * Expose method for moving time for Cypress.
-     * TODO OCT-1119: add explanation why it's here.
+     * In Cypress E2E tests some exclusive interactions with contracts are required.
+     * Currently, it's moving the epoch to the next one via JSON RPC API.
+     *
+     * These need to be exposed from within the application instead of being called from Cypress itself.
+     * Numerous attempts were made to call contracts via wagmiConfig.publicClient from Cypress,
+     * all of which were unsuccessful (1).
+     *
+     * The only working solution for the moment is exposing these calls from within the applicaiton.
+     *
+     * (1) History of commits here: https://github.com/golemfoundation/octant/pull/13.
      */
     if (window.Cypress) {
       // @ts-expect-error Left for debug purposes.
