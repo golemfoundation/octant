6eb85a4e1d2f48238ad53b08f6c9928668712d54
Łukasz Kujawski <lukas.kujawski@gmail.com>
2024-03-14 08:12:12 +0100

    fix: missing context in controller

--- backend/app/modules/withdrawals/controller.py
+++ backend/app/modules/withdrawals/controller.py
@@ -9,7 +9,9 @@ from app.modules.registry import get_services
 def get_withdrawable_eth(address: str) -> list[dict]:
     context = _get_context()
     service = get_services(context.epoch_state).withdrawals_service
-    return [dataclasses.asdict(w) for w in service.get_withdrawable_eth(address)]
+    return [
+        dataclasses.asdict(w) for w in service.get_withdrawable_eth(context, address)
+    ]
 
 
 def _get_context() -> Context:
