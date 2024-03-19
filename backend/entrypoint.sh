090bb3e75791dc9b6362fffa34dc1364689029db
Michał Kluczek <michal@wildland.io>
2024-03-19 21:19:01 +0100

    Add debug information to Python build image

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,5 +2,15 @@
 # Docker entrypoint for CI
 set -ex
 
+timeout --foreground -s TERM 60 bash -c \
+    'until [[ "$(dig +search +short -t A ${0})" != "" ]]; do\
+    >&2 echo "[-] Waiting for ${0} nslookup" && sleep 3;\
+    done' $(sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' <<< $SUBGRAPH_ENDPOINT)
+
+timeout --foreground -s TERM 60 bash -c \
+    'until [[ "$(dig +search +short -t A ${0})" != "" ]]; do\
+    >&2 echo "[-] Waiting for ${0} nslookup" && sleep 3;\
+    done' $(sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' <<< $ETH_RPC_PROVIDER_URL)
+
 poetry run flask db upgrade
 poetry run python startup.py
