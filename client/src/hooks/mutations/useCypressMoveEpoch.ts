0aaa4d939d0252401710bc54316656e0a4fc6e53
Andrzej Ziółek <a.j.ziolek@gmail.com>
2024-03-27 18:28:20 +0100

    test: checks

--- client/src/hooks/mutations/useCypressMoveEpoch.ts
+++ client/src/hooks/mutations/useCypressMoveEpoch.ts
@@ -75,7 +75,7 @@ export default function useCypressMoveEpoch(): UseMutationResult<boolean, unknow
 
         const timeToIncrease = moveTo === 'decisionWindowOpen'
           ? currentEpochAndAllocationTimestamps.timeCurrentEpochEnd! - blockTimestamp + 10 // [s]
-          : currentEpochAndAllocationTimestamps.timeCurrentEpochEnd! + currentEpochPropsTimestamps.decisionWindow - blockTimestamp + 10 // [s]
+          : currentEpochAndAllocationTimestamps.timeCurrentEpochEnd! + currentEpochProps.decisionWindow - blockTimestamp + 10 // [s]
 
         await wagmiConfig.publicClient.request({
           method: 'evm_increaseTime' as any,
