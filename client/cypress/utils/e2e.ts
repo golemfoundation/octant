38e74b90f3e3c8dd445b24031c01a67a0b39eb23
Andrzej Ziółek <a.j.ziolek@gmail.com>
2024-03-29 00:31:13 +0100

    test: in progress

--- client/cypress/utils/e2e.ts
+++ client/cypress/utils/e2e.ts
@@ -54,25 +54,25 @@ export const moveEpoch = async (
   cypressWindow: Cypress.AUTWindow,
   moveTo: 'decisionWindowClosed' | 'decisionWindowOpen',
 ): Promise<void> => {
-  cy.wrap(cypressWindow.mutateAsyncMoveToDecisionWindowOpen());
+  await cypressWindow.mutateAsyncMoveToDecisionWindowOpen();
   // Waiting 5s is a way to prevent the effects of slowing down the e2e environment (data update).
   cy.wait(5000);
   // reload is needed to get updated data in the app
   cy.reload();
   // Manually taking a pending snapshot after the epoch shift ensures that the snapshot is taken. Passing epoch multiple times without manually triggering pending snapshot in a short period of time may cause the e2e environment to fail.
-  cy.wrap(axios.post(`${env.serverEndpoint}snapshots/pending`));
+  await axios.post(`${env.serverEndpoint}snapshots/pending`);
   // Waiting 5s is a way to prevent the effects of slowing down the e2e environment (data update).
   cy.wait(5000);
   // reload is needed to get updated data in the app
   cy.reload();
 
   if (moveTo === 'decisionWindowClosed') {
-    cy.wrap(cypressWindow.mutateAsyncMoveToDecisionWindowClosed());
+    await cypressWindow.mutateAsyncMoveToDecisionWindowClosed();
     // Waiting 5s is a way to prevent the effects of slowing down the e2e environment (data update).
     cy.wait(5000);
     // reload is needed to get updated data in the app
     cy.reload();
-    cy.wrap(axios.post(`${env.serverEndpoint}snapshots/finalized`));
+    await axios.post(`${env.serverEndpoint}snapshots/finalized`);
     // Waiting 5s is a way to prevent the effects of slowing down the e2e environment (data update).
     cy.wait(5000);
     // reload is needed to get updated data in the app
