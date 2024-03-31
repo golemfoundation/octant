5685bf28c44cad80ff2c810fd5c8a70565c96e83
Andrzej Ziółek <a.j.ziolek@gmail.com>
2024-03-31 20:38:38 +0200

    test: move time before POST snapshots/finalized

--- client/cypress/utils/e2e.ts
+++ client/cypress/utils/e2e.ts
@@ -61,15 +61,17 @@ export const moveEpoch = (
     );
 
     if (isDecisionWindowOpen) {
-      axios.post(`${env.serverEndpoint}snapshots/finalized`).then(() => {
-        // Waiting 2s is a way to prevent the effects of slowing down the e2e environment (data update).
-        cy.wait(2000);
-        // reload is needed to get updated data in the app
-        cy.reload();
-        cy.get('[data-test=SyncView]', { timeout: 60000 }).should('not.exist');
-        // reload is needed to get updated data in the app
-        cy.reload();
-        resolve(true);
+      cypressWindow.mutateAsyncMoveToDecisionWindowClosed().then(() => {
+        axios.post(`${env.serverEndpoint}snapshots/finalized`).then(() => {
+          // Waiting 2s is a way to prevent the effects of slowing down the e2e environment (data update).
+          cy.wait(2000);
+          // reload is needed to get updated data in the app
+          cy.reload();
+          cy.get('[data-test=SyncView]', { timeout: 60000 }).should('not.exist');
+          // reload is needed to get updated data in the app
+          cy.reload();
+          resolve(true);
+        });
       });
     }
     resolve(true);
