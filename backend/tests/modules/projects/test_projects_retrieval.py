fa54dd7372f42e7acd53dbfb03971878429e26c5
tem.eth <artem.bv@pm.me>
2024-04-05 18:35:01 +0300

    fix: fix backend tests

--- backend/tests/modules/projects/test_projects_retrieval.py
+++ backend/tests/modules/projects/test_projects_retrieval.py
@@ -7,10 +7,10 @@ from tests.conftest import MOCK_PROPOSALS
 
 
 @pytest.fixture(autouse=True)
-def before(app):
+def before(app, patch_proposals):
     MOCK_PROPOSALS.get_proposal_addresses.return_value = ["0x0", "0x1"]
     MOCK_PROPOSALS.get_proposals_cid.return_value = (
-        "Qmds9N5y2vkMuPTD6M4EBxNXnf3bjTDmzWBGnCkQGsMMGe"
+        "QmXbFKrMGJUbXupmTQsQhoy9zkzXDBHZkPAzKC4yiaLt5n"
     )
     pass
 
@@ -25,6 +25,7 @@ def test_get_projects_metadata_epoch_1():
         projects_metadata.proposals_cid
         == "QmSQEFD35gKxdPEmngNt1CWe3kSwiiGqBn1Z3FZvWb8mvK"
     )
+    assert projects_metadata.projects_addresses == ["0x0", "0x1"]
 
 
 def test_get_projects_metadata_epoch_2():
@@ -36,4 +37,30 @@ def test_get_projects_metadata_epoch_2():
     assert (
         projects_metadata.proposals_cid
         == "Qmds9N5y2vkMuPTD6M4EBxNXnf3bjTDmzWBGnCkQGsMMGe"
+    )
+    assert projects_metadata.projects_addresses == ["0x0", "0x1"]
+
+
+def test_get_projects_metadata_epoch_3():
+    context = get_context(3)
+
+    service = StaticProjectsMetadataService()
+    projects_metadata: ProjectsMetadata = service.get_projects_metadata(context)
+
+    assert (
+        projects_metadata.proposals_cid
+        == "QmXbFKrMGJUbXupmTQsQhoy9zkzXDBHZkPAzKC4yiaLt5n"
+    )
+    assert projects_metadata.projects_addresses == ["0x0", "0x1"]
+
+
+def test_get_projects_metadata_epoch_100():
+    context = get_context(100)
+
+    service = StaticProjectsMetadataService()
+    projects_metadata: ProjectsMetadata = service.get_projects_metadata(context)
+
+    assert (
+        projects_metadata.proposals_cid
+        == "QmXbFKrMGJUbXupmTQsQhoy9zkzXDBHZkPAzKC4yiaLt5n"
     )
