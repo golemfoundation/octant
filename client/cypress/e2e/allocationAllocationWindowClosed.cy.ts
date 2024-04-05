adf5f1c0288568d56df978d82cb1969da73e5031
Andrzej Ziółek <a.j.ziolek@gmail.com>
2024-04-06 00:04:30 +0200

    test: debugging

--- client/cypress/e2e/allocationAllocationWindowClosed.cy.ts
+++ client/cypress/e2e/allocationAllocationWindowClosed.cy.ts
@@ -16,9 +16,9 @@ import { ROOT_ROUTES } from 'src/routes/RootRoutes/routes';
 
 let wasTimeMoved = false;
 
-[Object.values(viewports)[0]].forEach(({ device, viewportWidth, viewportHeight, isDesktop }) => {
+[Object.values(viewports)[0]].forEach(({ isDesktop }) => {
 
-  describe('', { testIsolation: false }, () => {
+  describe('move time', () => {
     before(() => {
       /**
        * Global Metamask setup done by Synpress is not always done.
@@ -29,106 +29,152 @@ let wasTimeMoved = false;
       cy.clock();
     });
 
-    describe('move time', () => {
+    it('allocation window is closed, when it is not, move time', () => {
+      cy.disconnectMetamaskWalletFromAllDapps();
+      mockCoinPricesServer();
+      localStorage.setItem(IS_ONBOARDING_ALWAYS_VISIBLE, 'false');
+      localStorage.setItem(IS_ONBOARDING_DONE, 'true');
+      localStorage.setItem(ALLOCATION_ITEMS_KEY, '[]');
+      visitWithLoader(ROOT_ROUTES.playground.absolute);
 
-      beforeEach(() => {
-        cy.disconnectMetamaskWalletFromAllDapps();
-        mockCoinPricesServer();
-        localStorage.setItem(IS_ONBOARDING_ALWAYS_VISIBLE, 'false');
-        localStorage.setItem(IS_ONBOARDING_DONE, 'true');
-        localStorage.setItem(ALLOCATION_ITEMS_KEY, '[]');
-        visitWithLoader(ROOT_ROUTES.playground.absolute);
-      });
+      cy.window().then(async win => {
+        const isDecisionWindowOpen = win.clientReactQuery.getQueryData(
+          QUERY_KEYS.isDecisionWindowOpen,
+        );
 
-      it('allocation window is closed, when it is not, move time', () => {
-        cy.window().then(async win => {
-          const isDecisionWindowOpen = win.clientReactQuery.getQueryData(
-            QUERY_KEYS.isDecisionWindowOpen,
-          );
+        cy.log(`test 1_1 ${isDecisionWindowOpen}`);
+        if (!isDecisionWindowOpen) {
+          expect(true).to.be.true;
+          return;
+        }
 
-          cy.log(`test 1_1 ${isDecisionWindowOpen}`);
-          if (!isDecisionWindowOpen) {
-            expect(true).to.be.true;
-            return;
-          }
-
-          // Move time only once, for the first device.
-          if (!wasTimeMoved) {
-            cy.log(`test 1_2 ${isDecisionWindowOpen}`);
-            cy.wrap(null).then(() => {
-              return moveEpoch(win, 'decisionWindowClosed').then(() => {
-                const isDecisionWindowOpenAfter = win.clientReactQuery.getQueryData(
-                  QUERY_KEYS.isDecisionWindowOpen,
-                );
-                wasTimeMoved = true;
-                cy.log(`test 1_2 ${isDecisionWindowOpenAfter}`);
-                expect(isDecisionWindowOpenAfter).to.be.false;
-              });
+        // Move time only once, for the first device.
+        if (!wasTimeMoved) {
+          cy.log(`test 1_2 ${isDecisionWindowOpen}`);
+          cy.wrap(null).then(() => {
+            return moveEpoch(win, 'decisionWindowClosed').then(() => {
+              const isDecisionWindowOpenAfter = win.clientReactQuery.getQueryData(
+                QUERY_KEYS.isDecisionWindowOpen,
+              );
+              wasTimeMoved = true;
+              cy.log(`test 1_2 ${isDecisionWindowOpenAfter}`);
+              expect(isDecisionWindowOpenAfter).to.be.false;
             });
-          } else {
-            expect(true).to.be.true;
-          }
-        });
+          });
+        } else {
+          expect(true).to.be.true;
+        }
       });
     });
-    // eslint-disable-next-line jest/no-disabled-tests
-    describe(
-      `allocation (allocation window closed): ${device}`,
-      { viewportHeight, viewportWidth },
-      () => {
-        beforeEach(() => {
-          cy.disconnectMetamaskWalletFromAllDapps();
-          mockCoinPricesServer();
-          localStorage.setItem(IS_ONBOARDING_ALWAYS_VISIBLE, 'false');
-          localStorage.setItem(IS_ONBOARDING_DONE, 'true');
-          localStorage.setItem(ALLOCATION_ITEMS_KEY, '[]');
-          visitWithLoader(ROOT_ROUTES.projects.absolute);
 
-          cy.get('[data-test^=ProjectItemSkeleton').should('not.exist');
-          cy.get('[data-test^=ProjectsView__ProjectsListItem]')
-            .eq(0)
-            .should('be.visible')
-            .find('[data-test=ProjectsListItem__name]')
-            .then($text => {
-              cy.wrap($text.text()).as('projectName');
-            });
+    it('AllocationItem shows all the elements', () => {
+      cy.disconnectMetamaskWalletFromAllDapps();
+      mockCoinPricesServer();
+      localStorage.setItem(IS_ONBOARDING_ALWAYS_VISIBLE, 'false');
+      localStorage.setItem(IS_ONBOARDING_DONE, 'true');
+      localStorage.setItem(ALLOCATION_ITEMS_KEY, '[]');
+      visitWithLoader(ROOT_ROUTES.projects.absolute);
 
-          cy.get('[data-test^=ProjectsView__ProjectsListItem')
-            .eq(0)
-            .find('[data-test=ProjectsListItem__ButtonAddToAllocate]')
-            .click();
-          navigateWithCheck(ROOT_ROUTES.allocation.absolute);
+      cy.get('[data-test^=ProjectItemSkeleton').should('not.exist');
+      cy.get('[data-test^=ProjectsView__ProjectsListItem]')
+        .eq(0)
+        .should('be.visible')
+        .find('[data-test=ProjectsListItem__name]')
+        .then($text => {
+          cy.wrap($text.text()).as('projectName');
         });
 
-        it('AllocationItem shows all the elements', () => {
-          connectWallet(true, false);
-          cy.get('[data-test=AllocationItem]')
-            .eq(0)
-            .find('[data-test=AllocationItem__name]')
-            .then($allocationItemName => {
-              cy.get('@projectName').then(projectName => {
-                expect(projectName).to.eq($allocationItemName.text());
-              });
-            });
+      cy.get('[data-test^=ProjectsView__ProjectsListItem')
+        .eq(0)
+        .find('[data-test=ProjectsListItem__ButtonAddToAllocate]')
+        .click();
+      navigateWithCheck(ROOT_ROUTES.allocation.absolute);
 
-          cy.get('[data-test=AllocationItem]')
-            .eq(0)
-            .find('[data-test=AllocationItem__imageProfile]')
-            .should(isDesktop ? 'be.visible' : 'not.be.visible');
-          cy.get('[data-test=AllocationItem]')
-            .eq(0)
-            .find('[data-test=AllocationItemRewards]')
-            .contains(isDesktop ? 'Threshold data unavailable' : 'No threshold data');
-          cy.get('[data-test=AllocationItem]')
-            .eq(0)
-            .find('[data-test=AllocationItem__InputText]')
-            .should('be.disabled');
-          cy.get('[data-test=AllocationItem]')
-            .eq(0)
-            .find('[data-test=AllocationItem__InputText__suffix]')
-            .contains('GWEI');
+      connectWallet(true, false);
+      cy.get('[data-test=AllocationItem]')
+        .eq(0)
+        .find('[data-test=AllocationItem__name]')
+        .then($allocationItemName => {
+          cy.get('@projectName').then(projectName => {
+            expect(projectName).to.eq($allocationItemName.text());
+          });
         });
-      },
-    );
+
+      cy.get('[data-test=AllocationItem]')
+        .eq(0)
+        .find('[data-test=AllocationItem__imageProfile]')
+        .should(isDesktop ? 'be.visible' : 'not.be.visible');
+      cy.get('[data-test=AllocationItem]')
+        .eq(0)
+        .find('[data-test=AllocationItemRewards]')
+        .contains(isDesktop ? 'Threshold data unavailable' : 'No threshold data');
+      cy.get('[data-test=AllocationItem]')
+        .eq(0)
+        .find('[data-test=AllocationItem__InputText]')
+        .should('be.disabled');
+      cy.get('[data-test=AllocationItem]')
+        .eq(0)
+        .find('[data-test=AllocationItem__InputText__suffix]')
+        .contains('GWEI');
+    });
   });
+  // eslint-disable-next-line jest/no-disabled-tests
+  // describe(
+  //   `allocation (allocation window closed): ${device}`,
+  //   { viewportHeight, viewportWidth },
+  //   () => {
+  //     beforeEach(() => {
+  //       cy.disconnectMetamaskWalletFromAllDapps();
+  //       mockCoinPricesServer();
+  //       localStorage.setItem(IS_ONBOARDING_ALWAYS_VISIBLE, 'false');
+  //       localStorage.setItem(IS_ONBOARDING_DONE, 'true');
+  //       localStorage.setItem(ALLOCATION_ITEMS_KEY, '[]');
+  //       visitWithLoader(ROOT_ROUTES.projects.absolute);
+  //
+  //       cy.get('[data-test^=ProjectItemSkeleton').should('not.exist');
+  //       cy.get('[data-test^=ProjectsView__ProjectsListItem]')
+  //         .eq(0)
+  //         .should('be.visible')
+  //         .find('[data-test=ProjectsListItem__name]')
+  //         .then($text => {
+  //           cy.wrap($text.text()).as('projectName');
+  //         });
+  //
+  //       cy.get('[data-test^=ProjectsView__ProjectsListItem')
+  //         .eq(0)
+  //         .find('[data-test=ProjectsListItem__ButtonAddToAllocate]')
+  //         .click();
+  //       navigateWithCheck(ROOT_ROUTES.allocation.absolute);
+  //     });
+  //
+  //     it('AllocationItem shows all the elements', () => {
+  //       connectWallet(true, false);
+  //       cy.get('[data-test=AllocationItem]')
+  //         .eq(0)
+  //         .find('[data-test=AllocationItem__name]')
+  //         .then($allocationItemName => {
+  //           cy.get('@projectName').then(projectName => {
+  //             expect(projectName).to.eq($allocationItemName.text());
+  //           });
+  //         });
+  //
+  //       cy.get('[data-test=AllocationItem]')
+  //         .eq(0)
+  //         .find('[data-test=AllocationItem__imageProfile]')
+  //         .should(isDesktop ? 'be.visible' : 'not.be.visible');
+  //       cy.get('[data-test=AllocationItem]')
+  //         .eq(0)
+  //         .find('[data-test=AllocationItemRewards]')
+  //         .contains(isDesktop ? 'Threshold data unavailable' : 'No threshold data');
+  //       cy.get('[data-test=AllocationItem]')
+  //         .eq(0)
+  //         .find('[data-test=AllocationItem__InputText]')
+  //         .should('be.disabled');
+  //       cy.get('[data-test=AllocationItem]')
+  //         .eq(0)
+  //         .find('[data-test=AllocationItem__InputText__suffix]')
+  //         .contains('GWEI');
+  //     });
+  //   },
+  // );
 });
