c9013efb0fe971f8e2dda088fee70fff5f30dcd7
Andrzej Ziółek <a.j.ziolek@gmail.com>
2024-04-11 00:14:11 +0200

    style: cleanup in progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -29,8 +29,7 @@ const App = (): ReactElement => {
 
   // useCypressHelpers needs to be called after all the initial sets done above.
   const { isFetching: isFetchingCypressHelpers } = useCypressHelpers();
-
-
+  
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { data: epochs } = useEpochs();
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
