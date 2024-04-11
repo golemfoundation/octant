e05a7b2273063739e2b92bcf8f5a5bfad7b3831b
Andrzej Ziółek <a.j.ziolek@gmail.com>
2024-04-11 07:50:31 +0200

    style: cleanup in progress

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -1,6 +1,3 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@synthetixio/synpress/support/commands';
-/* eslint-disable */
-import installLogsCollector from 'cypress-terminal-report/src/installLogsCollector';
-installLogsCollector();
 /* eslint-enable import/no-extraneous-dependencies */
