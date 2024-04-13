91c4ca7c3094a6924500948d7da38a004ad792bb
Andrzej Ziółek <a.j.ziolek@gmail.com>
2024-04-13 22:14:06 +0200

    test: add debug tools

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@synthetixio/synpress/support/commands';
-/* eslint-enable import/no-extraneous-dependencies */
 /* eslint-disable */
-require('cypress-terminal-report/src/installLogsCollector')();
+import installLogsCollector from 'cypress-terminal-report/src/installLogsCollector';
+installLogsCollector();
+/* eslint-enable import/no-extraneous-dependencies */
