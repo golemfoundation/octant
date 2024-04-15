55ea5db52b503943e9e4cc2a9ae7c1f2211df87c
Piotr Żelazko <pzelazko@golem.foundation>
2024-04-15 16:02:34 +0200

    chore: rebase flask migration

--- backend/migrations/versions/8ff293a1f737_add_leverage_field_to_allocation_request.py
+++ backend/migrations/versions/8ff293a1f737_add_leverage_field_to_allocation_request.py
@@ -1,7 +1,7 @@
 """add leverage column to allocation request
 
 Revision ID: 8ff293a1f737
-Revises: 71fa24bd9d4c
+Revises: fc7a01c20be5
 Create Date: 2024-04-05 11:44:37.629682
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "8ff293a1f737"
-down_revision = "71fa24bd9d4c"
+down_revision = "fc7a01c20be5"
 branch_labels = None
 depends_on = None
 
