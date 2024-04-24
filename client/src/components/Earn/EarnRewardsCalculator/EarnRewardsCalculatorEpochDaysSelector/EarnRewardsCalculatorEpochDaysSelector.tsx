8ababad199455b7f6dde6405e91e9b0eb5fca64f
Jakub Mikołajczyk <j.mikolajczyk96@gmail.com>
2024-04-24 15:50:35 +0200

    oct-1541: e2e

--- client/src/components/Earn/EarnRewardsCalculator/EarnRewardsCalculatorEpochDaysSelector/EarnRewardsCalculatorEpochDaysSelector.tsx
+++ client/src/components/Earn/EarnRewardsCalculator/EarnRewardsCalculatorEpochDaysSelector/EarnRewardsCalculatorEpochDaysSelector.tsx
@@ -17,21 +17,29 @@ const EarnRewardsCalculatorEpochDaysSelector: FC<EarnRewardsCalculatorEpochDaysS
   const epochDurationInDays = 90;
   const selectorOptions = [1, 2, 3]; // Epochs
 
+  const dataTest = 'EarnRewardsCalculatorEpochDaysSelector';
+
   return (
-    <div className={styles.root}>
-      <div className={styles.daysSelectorLabel}>{t('lockForEpoch', { count: numberOfEpochs })}</div>
+    <div className={styles.root} data-test={dataTest}>
+      <div className={styles.daysSelectorLabel} data-test={`${dataTest}__label`}>
+        {t('lockForEpoch', { count: numberOfEpochs })}
+      </div>
       <div className={styles.daysSelector}>
         <div className={styles.daysWrapper}>
           {selectorOptions.map(epoch => (
             <div
               key={epoch}
               className={cx(styles.day, numberOfEpochs === epoch && styles.isSelected)}
+              data-test={`${dataTest}__option--${epoch}`}
               onClick={() => onChange(epoch)}
             >
-              <span className={styles.dayLabel}>{epoch * epochDurationInDays}</span>
+              <span className={styles.dayLabel} data-test={`${dataTest}__optionLabel--${epoch}`}>
+                {epoch * epochDurationInDays}
+              </span>
               {numberOfEpochs === epoch ? (
                 <motion.div
                   className={styles.selectedItemBackground}
+                  data-test={`${dataTest}__optionBackground--${epoch}`}
                   layoutId="background"
                   transition={{ duration: 0.1 }}
                 />
@@ -39,7 +47,9 @@ const EarnRewardsCalculatorEpochDaysSelector: FC<EarnRewardsCalculatorEpochDaysS
             </div>
           ))}
         </div>
-        <div className={styles.label}>{i18n.t('common.days')}</div>
+        <div className={styles.suffix} data-test={`${dataTest}__suffix`}>
+          {i18n.t('common.days')}
+        </div>
       </div>
     </div>
   );
