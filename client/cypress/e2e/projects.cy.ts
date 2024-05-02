b049d6440a887304f2993fbda6492de1959711f2
Jakub Mikołajczyk <j.mikolajczyk96@gmail.com>
2024-05-02 09:57:23 +0200

    oct-1396: e2e projects.cy fix

--- client/cypress/e2e/projects.cy.ts
+++ client/cypress/e2e/projects.cy.ts
@@ -169,40 +169,40 @@ Object.values(viewports).forEach(({ device, viewportWidth, viewportHeight }) => 
         cy.get('[data-test=AllocationItem]').should('not.exist');
         cy.get('[data-test=Navbar__numberOfAllocations]').should('not.exist');
       });
+    });
 
-      it('ProjectsTimelineWidgetItem with href opens link when clicked without mouse movement', () => {
-        const milestones = getMilestones();
-        cy.get('[data-test=ProjectsTimelineWidget]').should('be.visible');
-        cy.get('[data-test=ProjectsTimelineWidgetItem]').should('have.length', milestones.length);
-        for (let i = 0; i < milestones.length; i++) {
-          if (milestones[i].href) {
-            cy.get('[data-test=ProjectsTimelineWidgetItem]')
-              .eq(i)
-              .within(() => {
-                cy.get('[data-test=ProjectsTimelineWidgetItem__Svg--arrowTopRight]').should(
-                  'be.visible',
-                );
-              });
+    it('ProjectsTimelineWidgetItem with href opens link when clicked without mouse movement', () => {
+      const milestones = getMilestones();
+      cy.get('[data-test=ProjectsTimelineWidget]').should('be.visible');
+      cy.get('[data-test=ProjectsTimelineWidgetItem]').should('have.length', milestones.length);
+      for (let i = 0; i < milestones.length; i++) {
+        if (milestones[i].href) {
+          cy.get('[data-test=ProjectsTimelineWidgetItem]')
+            .eq(i)
+            .within(() => {
+              cy.get('[data-test=ProjectsTimelineWidgetItem__Svg--arrowTopRight]').should(
+                'be.visible',
+              );
+            });
 
-            cy.get('[data-test=ProjectsTimelineWidgetItem]')
-              .eq(i)
-              .then(el => {
-                const { x } = el[0].getBoundingClientRect();
-                cy.get('[data-test=ProjectsTimelineWidgetItem]')
-                  .eq(i)
-                  .trigger('mousedown')
-                  .trigger('mouseup', { clientX: x + 10 });
-                cy.location('pathname').should('eq', ROOT_ROUTES.projects.absolute);
+          cy.get('[data-test=ProjectsTimelineWidgetItem]')
+            .eq(i)
+            .then(el => {
+              const { x } = el[0].getBoundingClientRect();
+              cy.get('[data-test=ProjectsTimelineWidgetItem]')
+                .eq(i)
+                .trigger('mousedown')
+                .trigger('mouseup', { clientX: x + 10 });
+              cy.location('pathname').should('eq', ROOT_ROUTES.projects.absolute);
 
-                cy.get('[data-test=ProjectsTimelineWidgetItem]')
-                  .eq(i)
-                  .trigger('mousedown')
-                  .trigger('mouseup');
-                cy.location('pathname').should('not.eq', ROOT_ROUTES.projects.absolute);
-              });
-          }
+              cy.get('[data-test=ProjectsTimelineWidgetItem]')
+                .eq(i)
+                .trigger('mousedown')
+                .trigger('mouseup');
+              cy.location('pathname').should('not.eq', ROOT_ROUTES.projects.absolute);
+            });
         }
-      });
+      }
     });
   });
 
