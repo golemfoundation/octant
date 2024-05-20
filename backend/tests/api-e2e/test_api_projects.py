281c5de8511ac9a78bd0af69aa865615357ce530
Mateusz Stolecki <mateusz@golem.foundation>
2024-05-20 09:06:01 +0200

    Black formatted
    Signed-off-by: Mateusz Stolecki <mateusz@golem.foundation>

--- backend/tests/api-e2e/test_api_projects.py
+++ backend/tests/api-e2e/test_api_projects.py
@@ -12,18 +12,18 @@ from tests.helpers.constants import STARTING_EPOCH
 def test_projects_basics(client: Client, setup_funds):
     projects_epoch1, status_code = client.get_projects(STARTING_EPOCH)
     projects_number_epoch1 = len(projects_epoch1["projectsAddresses"])
-    assert projects_number_epoch1 > (STARTING_EPOCH-1)
+    assert projects_number_epoch1 > (STARTING_EPOCH - 1)
     assert projects_epoch1["projectsCid"], "projectsCid is empty"
     assert status_code == 200
     app.logger.debug(f"Projects epoch 1:  {projects_epoch1}")
 
     # forward time to the beginning of the epoch 2
-    client.move_to_next_epoch(STARTING_EPOCH+1)
+    client.move_to_next_epoch(STARTING_EPOCH + 1)
     # wait for indexer to catch up
-    epoch_no = client.wait_for_sync(STARTING_EPOCH+1)
+    epoch_no = client.wait_for_sync(STARTING_EPOCH + 1)
     app.logger.debug(f"indexed epoch:  {epoch_no}")
 
-    projects_epoch2, status_code = client.get_projects(STARTING_EPOCH+1)
+    projects_epoch2, status_code = client.get_projects(STARTING_EPOCH + 1)
     projects_number_epoch2 = len(projects_epoch2["projectsAddresses"])
     assert projects_number_epoch2 > 0
     assert projects_epoch2["projectsCid"], "projectsCid is empty"
