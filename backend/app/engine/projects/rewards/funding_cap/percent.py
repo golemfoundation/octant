200369582f0490085256a7ad46496e1ac9dc62f7
kacper.golem <kacpergarbacinski@gmail.com>
2024-05-28 00:32:59 +0200

    chore: refactor

--- backend/app/engine/projects/rewards/funding_cap/percent.py
+++ backend/app/engine/projects/rewards/funding_cap/percent.py
@@ -10,6 +10,27 @@ from app.engine.projects.rewards.allocations import ProjectSumAllocationsDTO
 class FundingCapPercentCalculator:
     FUNDING_CAP_PERCENT: Decimal
 
+    def _calculate_surplus_to_project(
+        self,
+        cap_amount: Decimal,
+        capped_quadratic_distribution: pd.DataFrame,
+        uncapped_distribution: pd.DataFrame,
+    ) -> Decimal:
+        new_surplus = Decimal(0)
+        for project in uncapped_distribution.index:
+            if (
+                capped_quadratic_distribution[project] + uncapped_distribution[project]
+                > cap_amount
+            ):
+                new_surplus += (
+                    capped_quadratic_distribution[project]
+                    + uncapped_distribution[project]
+                ) - cap_amount
+                capped_quadratic_distribution[project] = cap_amount
+            else:
+                capped_quadratic_distribution[project] += uncapped_distribution[project]
+        return new_surplus
+
     def apply_capped_distribution(
         self,
         matched_allocated_by_addr: List[ProjectSumAllocationsDTO],
@@ -30,30 +51,16 @@ class FundingCapPercentCalculator:
                 capped_quadratic_distribution[project] = cap_amount
 
         while surplus > 0:
-            uncapped_proposals = capped_quadratic_distribution[
+            uncapped_projects = capped_quadratic_distribution[
                 capped_quadratic_distribution < cap_amount
             ]
             uncapped_distribution = (
-                uncapped_proposals / Decimal(uncapped_proposals.sum()) * surplus
+                uncapped_projects / Decimal(uncapped_projects.sum()) * surplus
             ).apply(Decimal)
 
-            new_surplus = 0
-            for proposal in uncapped_distribution.index:
-                if (
-                    capped_quadratic_distribution[proposal]
-                    + uncapped_distribution[proposal]
-                    > cap_amount
-                ):
-                    new_surplus += (
-                        capped_quadratic_distribution[proposal]
-                        + uncapped_distribution[proposal]
-                    ) - cap_amount
-                    capped_quadratic_distribution[proposal] = cap_amount
-                else:
-                    capped_quadratic_distribution[proposal] += uncapped_distribution[
-                        proposal
-                    ]
-
+            new_surplus = self._calculate_surplus_to_project(
+                cap_amount, capped_quadratic_distribution, uncapped_distribution
+            )
             surplus = new_surplus
 
         return capped_quadratic_distribution.to_dict()
