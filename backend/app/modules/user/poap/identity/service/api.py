83e57500eda8294ee058b81ff1a18a51af6df5ef
Łukasz Kujawski <lukas.kujawski@gmail.com>
2024-06-05 13:19:44 +0100

    feat: implement identity calls poap check with a db whitelist

--- backend/app/modules/user/poap/identity/service/api.py
+++
@@ -1,8 +0,0 @@
-from app.context.manager import Context
-from app.pydantic import Model
-
-
-class IdentityPoap(Model):
-    def has_poap(self, context: Context, address: str) -> bool:
-        # TODO implement identity poap check
-        ...
