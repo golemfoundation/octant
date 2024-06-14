98ce417fbbeeef46e2dcdc58e89fdfd928acbdf8
kacper.golem <kacpergarbacinski@gmail.com>
2024-06-14 13:26:18 +0200

    refactor: add docstr

--- backend/app/engine/projects/rewards/allocations/quadratic_funding.py
+++ backend/app/engine/projects/rewards/allocations/quadratic_funding.py
@@ -8,6 +8,11 @@ from app.engine.projects.rewards import ProjectAllocations
 
 class QuadraticFundingAllocations(ProjectAllocations):
     def _calc_allocations(self, allocations: List[AllocationItem]) -> Decimal:
+        """
+        Calculate quadratic funding for a project.
+        Returns the sum of the square roots of the uq_scores multiplied by the amounts.
+        The unit is in Decimal to avoid floating point errors but in the end the result is cast to int.
+        """
         sum_sqrts = sum(
             Decimal(sqrt(allocation.uq_score * allocation.amount))
             for allocation in allocations
