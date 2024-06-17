0cbc0f89ab069cf8a396a70a0e5af8d9b7a1cec3
Jakub Mikołajczyk <j.mikolajczyk96@gmail.com>
2024-06-17 17:58:03 +0200

    oct-1685: resolved conflicts

--- backend/tests/api-e2e/test_api_patron_mode.py
+++ backend/tests/api-e2e/test_api_patron_mode.py
@@ -7,19 +7,20 @@ from tests.helpers.signature import build_user_signature_patron
 
 
 @pytest.mark.api
-def test_patron_mode_basics(
+def test_withdrawals(
     client: Client,
     deployer: UserAccount,
     ua_alice: UserAccount,
+    ua_bob: UserAccount,
+    ua_carol: UserAccount,
     setup_funds,
 ):
     # lock GLM for one account
     ua_alice.lock(10000)
 
     # Check Alice's patron mode
-    alice_patron, status_code = client.get_patron_mode_status(ua_alice.address)
-    assert not alice_patron["status"], "Patron mode is enabled"
-    assert status_code == 200
+    alice_patron, _ = client.get_patron_mode_status(ua_alice.address)
+    print("Alice PATRON false: ", alice_patron)
 
     # forward time to the beginning of the epoch 2
     client.move_to_next_epoch(STARTING_EPOCH + 1)
@@ -32,35 +33,27 @@ def test_patron_mode_basics(
     res = client.pending_snapshot()
     assert res["epoch"] > 0
 
-    # Toggle patron mode on
-    signature = build_user_signature_patron(ua_alice._account, True)
-    res, status_code = client.patch_patron(ua_alice.address, signature.hex())
-    assert res["status"], "Patron mode is disabled"
-    assert status_code == 200
+    # Toggle patron mode
+    # SIGNATURE:
+    signature = build_user_signature_patron(ua_alice._account)
+    print("SIGNATURE: ", signature)
+    res, _ = client.patch_patron(ua_alice.address, signature.hex())
+    print("Patron toggled: ", res)
 
-    # Check patron list after alice patron mode on
-    patrons, status_code = client.get_epoch_patrons(STARTING_EPOCH)
-    for patron in patrons["patrons"]:
-        assert patron == ua_alice.address, "Patron user address is wrong"
-    assert status_code == 200
-
-    # Check Alice's patron mode
-    alice_patron, status_code = client.get_patron_mode_status(ua_alice.address)
-    assert alice_patron["status"], "Patron mode is disabled"
-    assert status_code == 200
-
-    # Toggle patron mode off
-    signature = build_user_signature_patron(ua_alice._account, False)
-    res, status_code = client.patch_patron(ua_alice.address, signature.hex())
-    assert not res["status"], "Patron mode is enabled"
-    assert status_code == 200
-
-    # Check patron list after alice patron mode off
-    patrons, status_code = client.get_epoch_patrons(STARTING_EPOCH)
-    assert len(patrons["patrons"]) == 0, "There are records in patron list"
-    assert status_code == 200
+    # Check patron list after alice patron
+    patrons, _ = client.get_epoch_patrons(STARTING_EPOCH)
+    print("Patrons in epoch 1: ", patrons)
 
     # Check Alice's patron mode
-    alice_patron, status_code = client.get_patron_mode_status(ua_alice.address)
-    assert not alice_patron["status"], "Patron mode is enabled"
-    assert status_code == 200
+    alice_patron, _ = client.get_patron_mode_status(ua_alice.address)
+    print("ALICE PATRON true ", alice_patron)
+
+    # TODO replace with helper to wait until end of voting
+    client.move_to_next_epoch(STARTING_EPOCH + 2)
+    epoch_no = client.wait_for_sync(STARTING_EPOCH + 2)
+    app.logger.debug(f"indexed epoch: {epoch_no}")
+
+    # make a finalized snapshot
+    # res = client.pending_snapshot()
+    res = client.finalized_snapshot()
+    assert res["epoch"] == STARTING_EPOCH
