495fb4af4fb50e1fca499357ffd945546c151ab1
Andrzej Ziółek <a.j.ziolek@gmail.com>
2024-06-21 22:34:17 +0200

    test: debug CYs, retries 0

--- client/synpress.config.ts
+++ client/synpress.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
     supportFile: 'cypress/support/index.ts',
   },
   numTestsKeptInMemory: 4,
+  retries: 0,
   video: true,
   viewportHeight: 1080,
   viewportWidth: 1920,
