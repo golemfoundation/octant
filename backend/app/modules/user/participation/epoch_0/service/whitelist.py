efde53d156c90f65fba6cd20e8026eccf1455cb5
Łukasz Kujawski <lukas.kujawski@gmail.com>
2024-06-21 15:01:32 +0200

    feat: updated uq logic volume 3

--- backend/app/modules/user/participation/epoch_0/service/whitelist.py
+++
@@ -1,8 +0,0 @@
-from app.context.manager import Context
-from app.infrastructure import database
-from app.pydantic import Model
-
-
-class WhitelistEpoch0(Model):
-    def exists(self, _: Context, address: str) -> bool:
-        return database.claims.claim_exists(address, exclude_sybils=True)
