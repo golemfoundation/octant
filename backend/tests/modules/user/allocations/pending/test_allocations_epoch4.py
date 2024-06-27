6f4db036353206ac61f3a7aaacefffdb9cc7d10e
Kacper Garbaciński <57113816+kgarbacinski@users.noreply.github.com>
2024-06-27 11:53:32 +0200

    OCT-1698: Cover Additional UT case for allocation (#282)
    ## Description
    
    ## Definition of Done
    
    1. [ ] Acceptance criteria are met.
    2. [ ] PR is manually tested before the merge by developer(s).
        - [ ] Happy path is manually checked.
    3. [ ] PR is manually tested by QA when their assistance is required
    (1).
    - [ ] Octant Areas & Test Cases are checked for impact and updated if
    required (2).
    4. [ ] Unit tests are added unless there is a reason to omit them.
    5. [ ] Automated tests are added when required.
    6. [ ] The code is merged.
    7. [ ] Tech documentation is added / updated, reviewed and approved
    (including mandatory approval by a code owner, should such exist for
    changed files).
        - [ ] BE: Swagger documentation is updated.
    8. [ ] When required by QA:
        - [ ] Deployed to the relevant environment.
        - [ ] Passed system tests.
    
    ---
    
    (1) Developer(s) in coordination with QA decide whether it's required.
    For small tickets introducing small changes QA assistance is most
    probably not required.
    
    (2) [Octant Areas & Test
    Cases](https://docs.google.com/spreadsheets/d/1cRe6dxuKJV3a4ZskAwWEPvrFkQm6rEfyUCYwLTYw_Cc).

--- backend/tests/modules/user/allocations/pending/test_allocations_epoch4.py
+++ backend/tests/modules/user/allocations/pending/test_allocations_epoch4.py
@@ -77,3 +77,42 @@ def test_simulate_allocation_with_user_uq_score(service, mock_users_db):
     ]
 
     assert service.get_user_allocation_sum(context, user1.address) == 100
+
+
+def test_simulate_allocation_user_uq_score_with_passed_param(service, mock_users_db):
+    context = get_context(epoch_num=4)
+    projects = context.projects_details.projects
+
+    (
+        user1,
+        _,
+        _,
+    ) = mock_users_db  # mock users with no scores since it'll be calculated in the service
+    make_user_allocation(context, user1)
+
+    next_allocations = [
+        AllocationDTO(projects[1], 200_000000000),
+    ]
+
+    leverage, threshold, rewards = service.simulate_allocation(
+        context, next_allocations, user1.address, LOW_UQ_SCORE
+    )
+    sorted_projects = sorted(projects)
+    assert leverage == 5502859957.887531
+    assert threshold is None
+    assert rewards == [
+        ProjectRewardDTO(sorted_projects[0], 0, 0),
+        ProjectRewardDTO(sorted_projects[1], 0, 0),
+        ProjectRewardDTO(
+            sorted_projects[2], 40000000000, int(MATCHED_REWARDS * LOW_UQ_SCORE)
+        ),
+        ProjectRewardDTO(sorted_projects[3], 0, 0),
+        ProjectRewardDTO(sorted_projects[4], 0, 0),
+        ProjectRewardDTO(sorted_projects[5], 0, 0),
+        ProjectRewardDTO(sorted_projects[6], 0, 0),
+        ProjectRewardDTO(sorted_projects[7], 0, 0),
+        ProjectRewardDTO(sorted_projects[8], 0, 0),
+        ProjectRewardDTO(sorted_projects[9], 0, 0),
+    ]
+
+    assert service.get_user_allocation_sum(context, user1.address) == 100
