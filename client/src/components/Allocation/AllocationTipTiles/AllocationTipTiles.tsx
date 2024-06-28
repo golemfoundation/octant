bfe1d23fcfaba94075e143bc0af6dfe86e4c9120
Andrzej Ziółek <a.j.ziolek@gmail.com>
2024-06-28 14:03:35 +0200

    feat: UQ too low tip tile

--- client/src/components/Allocation/AllocationTipTiles/AllocationTipTiles.tsx
+++ client/src/components/Allocation/AllocationTipTiles/AllocationTipTiles.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, Fragment } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useAccount } from 'wagmi';
 
@@ -7,6 +7,7 @@ import useMediaQuery from 'hooks/helpers/useMediaQuery';
 import useCurrentEpoch from 'hooks/queries/useCurrentEpoch';
 import useIndividualReward from 'hooks/queries/useIndividualReward';
 import useIsDecisionWindowOpen from 'hooks/queries/useIsDecisionWindowOpen';
+import useUqScore from 'hooks/queries/useUqScore';
 import useUserAllocations from 'hooks/queries/useUserAllocations';
 import useTipsStore from 'store/tips/store';
 
@@ -20,14 +21,25 @@ const AllocationTipTiles: FC<AllocationTipTilesProps> = ({ className }) => {
   const { data: isDecisionWindowOpen } = useIsDecisionWindowOpen();
   const { data: individualReward, isFetching: isFetchingIndividualReward } = useIndividualReward();
   const { data: userAllocations, isFetching: isFetchingUserAllocation } = useUserAllocations();
-  const { wasRewardsAlreadyClosed, setWasRewardsAlreadyClosed } = useTipsStore(state => ({
+  const { data: uqScore } = useUqScore(currentEpoch! - 1);
+  const {
+    wasRewardsAlreadyClosed,
+    setWasRewardsAlreadyClosed,
+    wasUqTooLowAlreadyClosed,
+    setWasUqTooLowAlreadyClosed,
+  } = useTipsStore(state => ({
     setWasConnectWalletAlreadyClosed: state.setWasConnectWalletAlreadyClosed,
     setWasRewardsAlreadyClosed: state.setWasRewardsAlreadyClosed,
+    setWasUqTooLowAlreadyClosed: state.setWasUqTooLowAlreadyClosed,
     wasConnectWalletAlreadyClosed: state.data.wasConnectWalletAlreadyClosed,
     wasRewardsAlreadyClosed: state.data.wasRewardsAlreadyClosed,
+    wasUqTooLowAlreadyClosed: state.data.wasUqTooLowAlreadyClosed,
   }));
 
   const isEpoch1 = currentEpoch === 1;
+
+  const isUqTooLowTipVisible =
+    !!isDecisionWindowOpen && uqScore === 20n && !wasUqTooLowAlreadyClosed;
 
   const isRewardsTipVisible =
     !isEpoch1 &&
@@ -41,15 +53,26 @@ const AllocationTipTiles: FC<AllocationTipTilesProps> = ({ className }) => {
     !wasRewardsAlreadyClosed;
 
   return (
-    <TipTile
-      className={className}
-      image="images/rewards.webp"
-      infoLabel={i18n.t('common.octantTips')}
-      isOpen={isRewardsTipVisible}
-      onClose={() => setWasRewardsAlreadyClosed(true)}
-      text={isDesktop ? t('rewards.text.desktop') : t('rewards.text.mobile')}
-      title={t('rewards.title')}
-    />
+    <Fragment>
+      <TipTile
+        className={className}
+        image="images/uqTooLow.webp"
+        infoLabel={i18n.t('common.octantTips')}
+        isOpen={isUqTooLowTipVisible}
+        onClose={() => setWasUqTooLowAlreadyClosed(true)}
+        text={isDesktop ? t('uqTooLow.text.desktop') : t('uqTooLow.text.mobile')}
+        title={isDesktop ? t('uqTooLow.title.desktop') : t('uqTooLow.title.mobile')}
+      />
+      <TipTile
+        className={className}
+        image="images/rewards.webp"
+        infoLabel={i18n.t('common.octantTips')}
+        isOpen={isRewardsTipVisible}
+        onClose={() => setWasRewardsAlreadyClosed(true)}
+        text={isDesktop ? t('rewards.text.desktop') : t('rewards.text.mobile')}
+        title={t('rewards.title')}
+      />
+    </Fragment>
   );
 };
 
