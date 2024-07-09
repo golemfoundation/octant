24f424a9d332fbc8da9e6d6aa484c93f217306db
Jakub Mikołajczyk <j.mikolajczyk96@gmail.com>
2024-07-09 15:12:43 +0200

    oct-1765: handling of uq score if user has no budget

--- client/src/api/clients/client-react-query.ts
+++ client/src/api/clients/client-react-query.ts
@@ -1,6 +1,7 @@
 import { MutationCache, QueryCache, QueryClient } from '@tanstack/react-query';
 
 import { handleError } from 'api/errorMessages';
+import { ROOTS } from 'api/queryKeys';
 
 const clientReactQuery = new QueryClient({
   defaultOptions: {
@@ -32,6 +33,10 @@ const clientReactQuery = new QueryClient({
   }),
   queryCache: new QueryCache({
     onError: (error, query) => {
+      if (query.queryKey.includes(ROOTS.uqScore)) {
+        return;
+      }
+
       // @ts-expect-error Error is of type 'unknown', but it is API or contract error.
       return handleError(error.reason, query);
     },
