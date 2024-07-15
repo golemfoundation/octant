7f2907e542bf36e14961f4b55a6059fb85e29932
kacper.golem <kacpergarbacinski@gmail.com>
2024-07-15 15:15:01 +0200

    chore: depends on chain type

--- backend/tests/modules/uq/conftest.py
+++ backend/tests/modules/uq/conftest.py
@@ -4,6 +4,7 @@ from unittest.mock import Mock
 import pytest
 
 from app.modules.uq.service.preliminary import PreliminaryUQ
+from tests.helpers.constants import UQ_THRESHOLD_MAINNET
 
 
 @pytest.fixture
@@ -15,4 +16,8 @@ def mock_antisybil():
 
 @pytest.fixture
 def service(mock_antisybil, mock_user_budgets):
-    return PreliminaryUQ(antisybil=mock_antisybil, budgets=mock_user_budgets)
+    return PreliminaryUQ(
+        antisybil=mock_antisybil,
+        budgets=mock_user_budgets,
+        uq_threshold=UQ_THRESHOLD_MAINNET,
+    )
