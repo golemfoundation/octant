92ae983ac1bdee77295f4d8cace91b0e36d6eb4a
Jakub Mikołajczyk <j.mikolajczyk96@gmail.com>
2024-07-26 14:06:25 +0200

    [HOTFIX]: multisig allocation

--- client/src/views/AllocationView/AllocationView.tsx
+++ client/src/views/AllocationView/AllocationView.tsx
@@ -277,15 +277,7 @@ const AllocationView = (): ReactElement => {
         value: '0',
       });
     }
-    if (isContract && !isProceedingToAllocateWithLowUQScore) {
-      setIsWaitingForFirstMultisigSignature(true);
-      toastService.showToast({
-        message: t('multisigSignatureToast.message'),
-        name: 'allocationMultisigInitialSignature',
-        title: t('multisigSignatureToast.title'),
-        type: 'warning',
-      });
-    }
+
     if (
       !userAllocations?.hasUserAlreadyDoneAllocation &&
       uqScore === 20n &&
@@ -294,8 +286,19 @@ const AllocationView = (): ReactElement => {
       setShowLowUQScoreModal(true);
       return;
     }
+
     if (isProceedingToAllocateWithLowUQScore) {
       setShowLowUQScoreModal(false);
+    }
+
+    if (isContract) {
+      setIsWaitingForFirstMultisigSignature(true);
+      toastService.showToast({
+        message: t('multisigSignatureToast.message'),
+        name: 'allocationMultisigInitialSignature',
+        title: t('multisigSignatureToast.title'),
+        type: 'warning',
+      });
     }
     allocateEvent.emit(allocationValuesNew, isManualMode);
   };
