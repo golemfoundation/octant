e876478ba1f8702f767a13ece9516e070bf0c143
kacper.golem <kacpergarbacinski@gmail.com>
2024-08-26 12:57:20 +0200

    OCT-1714: Check allocation for changes introduced in E4

--- backend/tests/api-e2e/test_api_allocations.py
+++ backend/tests/api-e2e/test_api_allocations.py
@@ -1,9 +1,9 @@
 import pytest
-
 from flask import current_app as app
+
 from app.legacy.core.projects import get_projects_addresses
 from tests.conftest import Client, UserAccount
-from tests.helpers.constants import STARTING_EPOCH
+from tests.helpers.constants import STARTING_EPOCH, LOW_UQ_SCORE
 
 
 @pytest.mark.api
@@ -49,24 +49,19 @@ def test_allocations(
     assert len(unique_proposals) == 3
 
 
-@pytest.mark.api
-def test_allocations_basics(
-    client: Client,
-    deployer: UserAccount,
-    ua_alice: UserAccount,
-    ua_bob: UserAccount,
-    setup_funds,
-):
+def _check_allocations_logic(client: Client, ua_alice: UserAccount, target_pending_epoch: int):
     alice_proposals = get_projects_addresses(1)[:3]
 
-    # lock GLM from one account
-    ua_alice.lock(10000)
+    i = 0
+    for i in range(0, target_pending_epoch + 1):
+        if i > 0:
+            client.move_to_next_epoch(STARTING_EPOCH + i)
 
-    # forward time to the beginning of the epoch 2
-    client.move_to_next_epoch(STARTING_EPOCH + 1)
+        if STARTING_EPOCH + i == target_pending_epoch:
+            ua_alice.lock(10000)
 
     # wait for indexer to catch up
-    epoch_no = client.wait_for_sync(STARTING_EPOCH + 1)
+    epoch_no = client.wait_for_sync(STARTING_EPOCH + i)
     app.logger.debug(f"indexed epoch: {epoch_no}")
 
     # make a snapshot
@@ -84,7 +79,7 @@ def test_allocations_basics(
         allocation_response_code == 201
     ), "Allocation status code is different than 201"
 
-    epoch_allocations, status_code = client.get_epoch_allocations(STARTING_EPOCH)
+    epoch_allocations, status_code = client.get_epoch_allocations(target_pending_epoch)
     assert len(epoch_allocations["allocations"]) == len(alice_proposals)
 
     for allocation in epoch_allocations["allocations"]:
@@ -97,14 +92,14 @@ def test_allocations_basics(
 
     # Check user donations
     user_allocations, status_code = client.get_user_allocations(
-        STARTING_EPOCH, ua_alice.address
+        target_pending_epoch, ua_alice.address
     )
     app.logger.debug(f"User allocations:  {user_allocations}")
     assert user_allocations["allocations"], "User allocations for given epoch are empty"
     assert status_code == 200, "Status code is different than 200"
 
     # Check donors
-    donors, status_code = client.get_donors(STARTING_EPOCH)
+    donors, status_code = client.get_donors(target_pending_epoch)
     app.logger.debug(f"Donors: {donors}")
     for donor in donors["donors"]:
         assert donor == ua_alice.address, "Donor address is wrong"
@@ -113,7 +108,7 @@ def test_allocations_basics(
     proposal_address = alice_proposals[0]
     # Check donors of particular proposal
     proposal_donors, status_code = client.get_proposal_donors(
-        STARTING_EPOCH, proposal_address
+        target_pending_epoch, proposal_address
     )
     app.logger.debug(f"Proposal donors: {proposal_donors}")
     for proposal_donor in proposal_donors:
@@ -133,3 +128,31 @@ def test_allocations_basics(
             assert matched["value"], "Leverage value is empty"
 
     assert status_code == 200, "Status code is different than 200"
+
+
+@pytest.mark.api
+def test_allocations_basics(
+    client: Client,
+    deployer: UserAccount,
+    ua_alice: UserAccount,
+    ua_bob: UserAccount,
+    setup_funds,
+):
+    _check_allocations_logic(client, ua_alice, target_pending_epoch=1)
+
+
+@pytest.mark.api
+def test_qf_and_uq_allocations(client: Client, ua_alice: UserAccount):
+    """
+    Test for QF and UQ allocations.
+    This test checks if we use the QF alongside with UQ functionality properly.
+    Introduced in E4.
+    """
+    PENDING_EPOCH = STARTING_EPOCH + 3
+
+    _check_allocations_logic(client, ua_alice, target_pending_epoch=PENDING_EPOCH)
+
+    # Check if UQ is saved in the database after the allocation properly
+    res, code = client.get_user_uq(ua_alice.address, 4)
+    assert code == 200
+    assert res["uniquenessQuotient"] == str(LOW_UQ_SCORE)
