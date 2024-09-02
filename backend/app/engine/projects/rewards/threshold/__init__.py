44a889f3186fadc5cf48305e1faeef05e7611404
Kacper Garbaciński <57113816+kgarbacinski@users.noreply.github.com>
2024-09-02 14:36:01 +0200

    OCT-1837 & OCT-1838: Upgrade metrics pie chart for unused MR (#383)
    ## Description
    
    ## Definition of Done
    
    1. [ ] Acceptance criteria are met.
    2. [ ] PR is manually tested before the merge by developer(s).
        - [ ] Happy path is manually checked.
    3. [ ] PR is manually tested by QA when their assistance is required
    (1).
    - [ ] Octant Areas & Test Cases are checked for impact and updated if
    required (2).
    4. [ ] Unit tests are added unless there is a reason to omit them.
    5. [ ] Automated tests are added when required.
    6. [ ] The code is merged.
    7. [ ] Tech documentation is added / updated, reviewed and approved
    (including mandatory approval by a code owner, should such exist for
    changed files).
        - [ ] BE: Swagger documentation is updated.
    8. [ ] When required by QA:
        - [ ] Deployed to the relevant environment.
        - [ ] Passed system tests.
    
    ---
    
    (1) Developer(s) in coordination with QA decide whether it's required.
    For small tickets introducing small changes QA assistance is most
    probably not required.
    
    (2) [Octant Areas & Test
    Cases](https://docs.google.com/spreadsheets/d/1cRe6dxuKJV3a4ZskAwWEPvrFkQm6rEfyUCYwLTYw_Cc).
    
    ---------
    
    Co-authored-by: Andrzej Ziółek <a.j.ziolek@gmail.com>

--- backend/app/engine/projects/rewards/threshold/__init__.py
+++ backend/app/engine/projects/rewards/threshold/__init__.py
@@ -1,5 +1,9 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import List, Dict
+
+from app.engine.projects.rewards import AllocationsBelowThreshold
+from app.engine.projects.rewards import AllocationItem
 
 
 @dataclass
@@ -12,4 +16,10 @@ class ProjectThresholdPayload:
 class ProjectThreshold(ABC):
     @abstractmethod
     def calculate_threshold(self, payload: ProjectThresholdPayload) -> int:
+        pass
+
+    @abstractmethod
+    def get_total_allocations_below_threshold(
+        self, allocations: Dict[str, List[AllocationItem]]
+    ) -> AllocationsBelowThreshold:
         pass
