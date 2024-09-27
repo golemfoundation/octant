1d5720ae6985e8caa5f3b712bdb9ed4fde84577d
kacper.golem <kacpergarbacinski@gmail.com>
2024-09-27 11:29:06 +0200

    OCT-1922: Extend searching functionality for multiple params

--- backend/tests/modules/projects/details/test_projects_filtering.py
+++ backend/tests/modules/projects/details/test_projects_filtering.py
@@ -14,7 +14,8 @@ def before(app, patch_projects_details):
 
 @pytest.mark.parametrize("search_phrase", ["Octant", "TEST", "AnyName"])
 def test_get_projects_details_by_search_phrase(search_phrase):
-    context = get_context(4)
+    epoch = 4
+    context = get_context(epoch)
     service = StaticProjectsDetailsService()
     projects_details: ProjectsDetailsDTO = (
         service.get_projects_details_by_search_phrase(context, search_phrase)
@@ -22,6 +23,8 @@ def test_get_projects_details_by_search_phrase(search_phrase):
 
     for project in projects_details.projects_details:
         assert search_phrase.lower() in project["name"].lower()
+        assert project["epoch"] == str(epoch)
+        assert "address" in project and project["address"] is not None
 
 
 def test_get_projects_details_by_search_phrase_not_found():
