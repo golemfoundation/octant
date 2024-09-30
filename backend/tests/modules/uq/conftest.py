f8366558b2fbe01946948ba688b835d9f362e7ea
kacper.golem <kacpergarbacinski@gmail.com>
2024-09-30 16:09:24 +0200

    align with uts

--- backend/tests/modules/uq/conftest.py
+++ backend/tests/modules/uq/conftest.py
@@ -4,13 +4,16 @@ from unittest.mock import Mock
 import pytest
 
 from app.modules.uq.service.preliminary import PreliminaryUQ
+from app.modules.user.antisybil.dto import AntisybilStatusDTO
 from tests.helpers.constants import UQ_THRESHOLD_MAINNET
 
 
 @pytest.fixture
 def mock_antisybil():
     mock = Mock()
-    mock.get_antisybil_status.return_value = (10.0, datetime.now())
+    mock.get_antisybil_status.return_value = AntisybilStatusDTO(
+        score=10.0, expires_at=datetime.now(), is_on_timeout_list=False
+    )
     return mock
 
 
