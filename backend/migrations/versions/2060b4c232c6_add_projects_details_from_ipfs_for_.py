bd7eb38f51821264ea239970a11af039d049e7fc
kacper.golem <kacpergarbacinski@gmail.com>
2024-10-10 11:08:27 +0200

    blackify

--- backend/migrations/versions/2060b4c232c6_add_projects_details_from_ipfs_for_.py
+++ backend/migrations/versions/2060b4c232c6_add_projects_details_from_ipfs_for_.py
@@ -7,12 +7,10 @@ Create Date: 2024-10-09 22:58:18.845671
 """
 from migrations.ipfs_integration import migration_helpers as ipfs_migration
 
-
 revision = "2060b4c232c6"
 down_revision = "c34003767fa8"
 branch_labels = None
 depends_on = None
-
 
 FILENAME = "ipfs_projects_details_epoch_{}.json"
 EPOCHS = (5,)
@@ -28,4 +26,3 @@ def downgrade():
     for epoch in EPOCHS:
         filename = FILENAME.format(epoch)
         ipfs_migration.downgrade(filename, epoch)
-
