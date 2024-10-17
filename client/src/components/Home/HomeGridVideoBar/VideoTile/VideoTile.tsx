561b6a33870591cdec17fae4b025857c9259003b
Jakub Mikołajczyk <j.mikolajczyk96@gmail.com>
2024-10-17 11:24:10 +0200

    oct-1875: vimeo controls

--- client/src/components/Home/HomeGridVideoBar/VideoTile/VideoTile.tsx
+++ client/src/components/Home/HomeGridVideoBar/VideoTile/VideoTile.tsx
@@ -1,22 +1,18 @@
 import Player from '@vimeo/player';
 import cx from 'classnames';
 import { useInView } from 'framer-motion';
-import React, { FC, useRef, useState } from 'react';
-
-import Svg from 'components/ui/Svg';
-import { play } from 'svg/misc';
+import React, { FC, useRef } from 'react';
 
 import VideoTileProps from './types';
 import styles from './VideoTile.module.scss';
 
 const VideoTile: FC<VideoTileProps> = ({ title, author, url }) => {
   const ref = useRef(null);
-  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
   const playerRef = useRef<Player>();
   const isInView = useInView(ref, { amount: 'all' });
 
   // https://developer.vimeo.com/player/sdk/embed
-  const urlWithOptions = `${url}&controls=false&dnt=true&muted=true`;
+  const urlWithOptions = `${url}&dnt=true&muted=true`;
 
   return (
     <div ref={ref} className={cx(styles.root, isInView && styles.isInView)}>
@@ -27,14 +23,6 @@ const VideoTile: FC<VideoTileProps> = ({ title, author, url }) => {
           }
           const player = new Player(iframeRef);
           playerRef.current = player;
-
-          player.on('play', () => {
-            setIsVideoPlaying(true);
-          });
-
-          player.on('pause', () => {
-            setIsVideoPlaying(false);
-          });
 
           // workaround to skip thumbnail
           player.on('loaded', () => {
@@ -52,25 +40,6 @@ const VideoTile: FC<VideoTileProps> = ({ title, author, url }) => {
         src={urlWithOptions}
         title={title}
       />
-      <div
-        className={cx(styles.videoOverlay, isVideoPlaying && styles.isVideoPlaying)}
-        onClick={() => {
-          if (!isVideoPlaying) {
-            return;
-          }
-          playerRef.current?.pause();
-        }}
-      />
-      {!isVideoPlaying && (
-        <Svg
-          classNameSvg={styles.playButton}
-          img={play}
-          onClick={() => {
-            playerRef.current?.play();
-          }}
-          size={[3.4, 2.4]}
-        />
-      )}
       <div className={styles.info}>
         <div className={styles.title}>{title}</div>
         <div className={styles.subtitle}>{author}</div>
