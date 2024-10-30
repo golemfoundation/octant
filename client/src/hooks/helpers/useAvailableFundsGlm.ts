3be32e2aa0b3185265fd4d0fe6930ad51d85f3e2
Andrzej Ziółek <a.j.ziolek@gmail.com>
2024-10-30 20:35:16 +0100

    style: revert changes to currency hooks

--- client/src/hooks/helpers/useAvailableFundsGlm.ts
+++ client/src/hooks/helpers/useAvailableFundsGlm.ts
@@ -1,5 +1,8 @@
-import { useAccount, useBalance, UseBalanceReturnType } from 'wagmi';
+import { useQueryClient } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { useAccount, useBalance, UseBalanceReturnType, useBlockNumber } from 'wagmi';
 
+import networkConfig from 'constants/networkConfig';
 import env from 'env';
 
 export default function useAvailableFundsGlm(): UseBalanceReturnType<{
@@ -9,8 +12,29 @@ export default function useAvailableFundsGlm(): UseBalanceReturnType<{
   value: bigint;
 }> {
   const { address } = useAccount();
-  return useBalance({
+  const { data: blockNumber } = useBlockNumber({ chainId: networkConfig.id, watch: true });
+  const queryClient = useQueryClient();
+  const balance = useBalance({
     address,
     token: env.contractGlmAddress as `0x{${string}}`,
   });
+
+  /**
+   * In local environment blocks are mined very fast, flooding the logs.
+   * Hence, disable refetch on each block.
+   */
+  const isCypressLocalRun = env.network === 'Local' && !!window.Cypress;
+
+  // balance.queryKey is an array. Stingifying it is required for useEffect to compare it.
+  const balanceQueryKeyStringified = JSON.stringify(balance.queryKey);
+
+  useEffect(() => {
+    if (isCypressLocalRun) {
+      return;
+    }
+    queryClient.invalidateQueries({ queryKey: balance.queryKey });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isCypressLocalRun, blockNumber, balanceQueryKeyStringified]);
+
+  return balance;
 }
