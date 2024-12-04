69131b93c2c64cc08959ec9cacd6fd7a56fe1978
Jakub Mikołajczyk <j.mikolajczyk96@gmail.com>
2024-12-04 19:48:38 +0000

    oct-2125: settings e2e test

--- client/cypress/e2e/_12settings.cy.ts
+++ client/cypress/e2e/_12settings.cy.ts
@@ -262,7 +262,6 @@ Object.values(viewports).forEach(
         cy.getAllLocalStorage().then(() => {
           expect(localStorage.getItem(IS_ONBOARDING_ALWAYS_VISIBLE)).eq('true');
         });
-
         cy.reload();
         cy.get('[data-test=ModalOnboarding]').should('be.visible');
         cy.get('[data-test=ModalOnboarding__Button]').click();
