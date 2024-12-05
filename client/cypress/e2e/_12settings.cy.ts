eb75df7f1b5067b41943d12db9d7eaa6a2cbf9b1
Jakub Mikołajczyk <j.mikolajczyk96@gmail.com>
2024-12-05 21:39:06 +0000

    oct-2129: remove empty lines

--- client/cypress/e2e/_12settings.cy.ts
+++ client/cypress/e2e/_12settings.cy.ts
@@ -263,7 +263,6 @@ Object.values(viewports).forEach(
           expect(localStorage.getItem(IS_ONBOARDING_ALWAYS_VISIBLE)).eq('true');
         });
         cy.reload();
-
         cy.get('[data-test=ModalOnboarding]').should('be.visible');
         cy.get('[data-test=ModalOnboarding__Button]').click();
 
@@ -278,7 +277,6 @@ Object.values(viewports).forEach(
         cy.getAllLocalStorage().then(() => {
           expect(localStorage.getItem(IS_ONBOARDING_ALWAYS_VISIBLE)).eq('false');
         });
-
         cy.reload();
         cy.get('[data-test=ModalOnboarding]').should('not.exist');
 
