56bac81f4b02bc266a96543a4af489a8013a8a1d
kgarbacinski <kacpergarbacinski@gmail.com>
2024-12-23 09:33:53 +0100

    check regression

--- backend/v2/project_rewards/services.py
+++ backend/v2/project_rewards/services.py
@@ -8,7 +8,7 @@ from v2.project_rewards.capped_quadriatic import (
     capped_quadriatic_funding,
 )
 from v2.projects.contracts import ProjectsContracts
-
+import asyncio
 
 @dataclass
 class ProjectRewardsEstimator:
@@ -22,15 +22,15 @@ class ProjectRewardsEstimator:
 
     async def get(self) -> CappedQuadriaticFunding:
         # Gather all the necessary data for the calculation
-        # all_projects, matched_rewards, allocations = await asyncio.gather(
-        #     self.projects_contracts.get_project_addresses(self.epoch_number),
-        #     self.matched_rewards_estimator.get(),
-        #     get_allocations_with_user_uqs(self.session, self.epoch_number),
-        # )
+        all_projects, matched_rewards, allocations = await asyncio.gather(
+            self.projects_contracts.get_project_addresses(self.epoch_number),
+            self.matched_rewards_estimator.get(),
+            get_allocations_with_user_uqs(self.session, self.epoch_number),
+        )
 
-        all_projects = await self.projects_contracts.get_project_addresses(self.epoch_number)
-        matched_rewards = await self.matched_rewards_estimator.get()
-        allocations = await get_allocations_with_user_uqs(self.session, self.epoch_number)
+        # all_projects = await self.projects_contracts.get_project_addresses(self.epoch_number)
+        # matched_rewards = await self.matched_rewards_estimator.get()
+        # allocations = await get_allocations_with_user_uqs(self.session, self.epoch_number)
 
         # Calculate using the Capped Quadriatic Funding formula
         return capped_quadriatic_funding(
