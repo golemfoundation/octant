ce9ee74811ca829d13dd4a22c3b23358d5a08a7c
Jakub Mikołajczyk <j.mikolajczyk96@gmail.com>
2025-01-03 13:53:24 +0100

    oct-2097: social media buttons

--- client/src/components/shared/Layout/LayoutFooter/LayoutFooter.tsx
+++ client/src/components/shared/Layout/LayoutFooter/LayoutFooter.tsx
@@ -2,6 +2,7 @@ import cx from 'classnames';
 import React, { FC, memo, useLayoutEffect, useRef } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 
+import Button from 'components/ui/Button';
 import Svg from 'components/ui/Svg';
 import {
   BLOG_POST,
@@ -12,43 +13,33 @@ import {
   OCTANT_BUILD_LINK,
   OCTANT_DOCS,
   TERMS_OF_USE,
-  // TODO OCT-2097 Bring Twitter link back.
-  // TWITTER_LINK,
   PRIVACY_POLICY,
+  X_LINK,
 } from 'constants/urls';
 import useMediaQuery from 'hooks/helpers/useMediaQuery';
-import { octantSemiTransparent } from 'svg/logo';
+import { discord, farcaster, octantSemiTransparent, X } from 'svg/logo';
 
 import styles from './LayoutFooter.module.scss';
 import LayoutFooterProps from './types';
 
 const LayoutFooter: FC<LayoutFooterProps> = ({ className }) => {
   const { t } = useTranslation('translation', { keyPrefix: 'layout.footer' });
-  const { isDesktop } = useMediaQuery();
+  const { isMobile } = useMediaQuery();
   const newsletterRef = useRef<HTMLDivElement>(null);
   const dataTestRoot = 'LayoutFooter';
 
-  const links = isDesktop
-    ? [
-        { id: 'website', label: t('links.website'), link: OCTANT_BUILD_LINK },
-        { id: 'docs', label: t('links.docs'), link: OCTANT_DOCS },
-        { id: 'blog', label: t('links.blog'), link: BLOG_POST },
-        { id: 'discord', label: t('links.discord'), link: DISCORD_LINK },
-        { id: 'farcaster', label: t('links.farcaster'), link: FARCASTER_LINK },
-        // TODO OCT-2097 Bring Twitter/X back.
-        // { label: t('links.twitterX'), link: TWITTER_LINK },
-        { id: 'brandAssets', label: t('links.brandAssets'), link: BRAND_ASSETS_FIGMA_LINK },
-        { id: 'privacyPolicy', label: t('links.privacyPolicy'), link: PRIVACY_POLICY },
-        { id: 'termsOfUse', label: t('links.termsOfUse'), link: TERMS_OF_USE },
-      ]
-    : [
-        { id: 'website', label: t('links.website'), link: OCTANT_BUILD_LINK },
-        { id: 'docs', label: t('links.docs'), link: OCTANT_DOCS },
-        { id: 'farcaster', label: t('links.farcaster'), link: FARCASTER_LINK },
-        { id: 'discord', label: t('links.discord'), link: DISCORD_LINK },
-        { id: 'privacyPolicy', label: t('links.privacyPolicy'), link: PRIVACY_POLICY },
-        { id: 'termsOfUse', label: t('links.termsOfUse'), link: TERMS_OF_USE },
-      ];
+  const links = [
+    { id: 'website', label: t('links.website'), link: OCTANT_BUILD_LINK },
+    { id: 'docs', label: t('links.docs'), link: OCTANT_DOCS },
+    { id: 'blog', label: t('links.blog'), link: BLOG_POST },
+    {
+      id: 'brandAssets',
+      label: isMobile ? t('links.brand') : t('links.brandAssets'),
+      link: BRAND_ASSETS_FIGMA_LINK,
+    },
+    { id: 'privacyPolicy', label: t('links.privacyPolicy'), link: PRIVACY_POLICY },
+    { id: 'termsOfUse', label: t('links.termsOfUse'), link: TERMS_OF_USE },
+  ];
 
   useLayoutEffect(() => {
     if (!newsletterRef.current || newsletterRef.current.children.length || window.Cypress) {
@@ -112,6 +103,29 @@ const LayoutFooter: FC<LayoutFooterProps> = ({ className }) => {
         <div className={styles.newsletterText} data-test={`${dataTestRoot}__newsletterText`}>
           {t('newsletterText')}
         </div>
+      </div>
+      <div className={styles.socialMedia}>
+        <Button
+          className={styles.socialMediaSvg}
+          dataTest="LayoutFooter__Button--x"
+          href={X_LINK}
+          Icon={<Svg img={X} size={3.2} />}
+          variant="iconOnly"
+        />
+        <Button
+          className={styles.socialMediaSvg}
+          dataTest="LayoutFooter__Button--farcaster"
+          href={FARCASTER_LINK}
+          Icon={<Svg img={farcaster} size={3.2} />}
+          variant="iconOnly"
+        />
+        <Button
+          className={styles.socialMediaSvg}
+          dataTest="LayoutFooter__Button--discord"
+          href={DISCORD_LINK}
+          Icon={<Svg img={discord} size={3.2} />}
+          variant="iconOnly"
+        />
       </div>
     </div>
   );
