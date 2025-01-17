e37fcedf2178de0ddb90b85622c0c666ea09ff4d
Jakub Mikołajczyk <j.mikolajczyk96@gmail.com>
2025-01-17 01:29:26 +0100

    oct-2136: e2e uq fix

--- client/cypress/e2e/_23uq.cy.ts
+++ client/cypress/e2e/_23uq.cy.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import chaiColors from 'chai-colors';
 
-import { connectWallet, mockCoinPricesServer, visitWithLoader } from 'cypress/utils/e2e';
+import { mockCoinPricesServer, visitWithLoader } from 'cypress/utils/e2e';
 import viewports from 'cypress/utils/viewports';
 import {
   HAS_ONBOARDING_BEEN_CLOSED,
@@ -16,43 +16,40 @@ Object.values(viewports).forEach(({ device, viewportWidth, viewportHeight }) => 
   describe(`[AW IS OPEN] UQ: ${device}`, { viewportHeight, viewportWidth }, () => {
     before(() => {
       cy.clearLocalStorage();
-      /**
-       * Global Metamask setup done by Synpress is not always done.
-       * Since Synpress needs to have valid provider to fetch the data from contracts,
-       * setupMetamask is required in each test suite.
-       */
-      cy.setupMetamask();
     });
 
     beforeEach(() => {
-      cy.disconnectMetamaskWalletFromAllDapps();
       mockCoinPricesServer();
       localStorage.setItem(IS_ONBOARDING_ALWAYS_VISIBLE, 'false');
       localStorage.setItem(IS_ONBOARDING_DONE, 'true');
       localStorage.setItem(HAS_ONBOARDING_BEEN_CLOSED, 'true');
       visitWithLoader(ROOT_ROUTES.home.absolute);
-      connectWallet({ isPatronModeEnabled: false });
     });
 
-    it('Each element of UQ tile is visible and has correct value', () => {
-      cy.get('[data-test=HomeGridUQScore__Button--whatIsThis]').should('be.visible');
-      cy.get('[data-test=HomeGridUQScore__Button--whatIsThis]').click();
-      cy.wait(500);
-      cy.get('[data-test=ModalCalculatingYourUniqueness]').should('be.visible');
-      cy.get('[data-test=ModalCalculatingYourUniqueness__ProgressStepperSlim__element]').should(
-        'have.length',
-        3,
-      );
-      cy.get('[data-test=ModalCalculatingYourUniqueness__Button]').click();
-      cy.wait(500);
-      cy.get('[data-test=ModalCalculatingYourUniqueness]').should('not.exist');
-      cy.get('[data-test=HomeGridUQScoreAddresses]').should('be.visible');
-      cy.get('[data-test=HomeGridUQScore__Button--scoreTooLow]').should('be.visible');
-      cy.get('[data-test=HomeGridUQScore__Button--scoreTooLow]')
-        .invoke('attr', 'href')
-        .should('eq', 'https://passport.gitcoin.co/#/octant/');
-      cy.get('[data-test=HomeGridUQScore__Button--recalculate]').should('be.visible');
-      cy.get('[data-test=HomeGridUQScore__Button--delegate]').should('be.visible');
-    });
+    it(
+      'Each element of UQ tile is visible and has correct value',
+      { scrollBehavior: false },
+      () => {
+        cy.get('[data-test=HomeGridUQScore]').scrollIntoView({ offset: { left: 0, top: -100 } });
+        cy.get('[data-test=HomeGridUQScore__Button--whatIsThis]').should('be.visible');
+        cy.get('[data-test=HomeGridUQScore__Button--whatIsThis]').click();
+        cy.wait(500);
+        cy.get('[data-test=ModalCalculatingYourUniqueness]').should('be.visible');
+        cy.get('[data-test=ModalCalculatingYourUniqueness__ProgressStepperSlim__element]').should(
+          'have.length',
+          3,
+        );
+        cy.get('[data-test=ModalCalculatingYourUniqueness__Button]').click();
+        cy.wait(500);
+        cy.get('[data-test=ModalCalculatingYourUniqueness]').should('not.exist');
+        cy.get('[data-test=HomeGridUQScoreAddresses]').should('be.visible');
+        cy.get('[data-test=HomeGridUQScore__Button--scoreTooLow]').should('be.visible');
+        cy.get('[data-test=HomeGridUQScore__Button--scoreTooLow]')
+          .invoke('attr', 'href')
+          .should('eq', 'https://passport.gitcoin.co/#/octant/');
+        cy.get('[data-test=HomeGridUQScore__Button--recalculate]').should('be.visible');
+        cy.get('[data-test=HomeGridUQScore__Button--delegate]').should('be.visible');
+      },
+    );
   });
 });
