a19dcc5eb993ea1ad353d03bc6cb2a7da304ee37
kgarbacinski <kacpergarbacinski@gmail.com>
2025-01-21 14:01:37 +0100

    OCT-2330: Fix effective_deposit

--- backend/app/modules/user/events_generator/core.py
+++ backend/app/modules/user/events_generator/core.py
@@ -4,6 +4,40 @@ from typing import List
 from app.engine.user.effective_deposit import DepositEvent, EventType, DepositSource
 
 
+def _remove_redundant_events_within_grace_period(
+    modified_events: List[DepositEvent], sablier_unlock_grace_period: int
+) -> List[DepositEvent]:
+    i = 0
+    while i < len(modified_events) - 1:
+        current_event = modified_events[i]
+        next_event = modified_events[i + 1]
+
+        is_unlock_lock_pair = (
+            current_event.type == EventType.UNLOCK
+            and next_event.type == EventType.LOCK
+            and current_event.source == DepositSource.SABLIER
+            and next_event.source == DepositSource.OCTANT
+            and next_event.timestamp - current_event.timestamp
+            < sablier_unlock_grace_period
+        )
+
+        if is_unlock_lock_pair:
+            if current_event.amount == next_event.amount:
+                del modified_events[i : i + 2]
+                continue
+            elif current_event.amount < next_event.amount:
+                excessive_amount = next_event.amount - current_event.amount
+                next_event.amount = excessive_amount
+                next_event.deposit_before = current_event.deposit_before
+                next_event.deposit_after = next_event.deposit_before + excessive_amount
+                del modified_events[i]
+                continue
+
+        i += 1
+
+    return modified_events
+
+
 def unify_deposit_balances(
     events: List[DepositEvent], sablier_unlock_grace_period: int
 ) -> List[DepositEvent]:
@@ -19,67 +53,27 @@ def unify_deposit_balances(
     acc_balance_sablier = 0
     acc_balance_octant = events[0].deposit_before  # balance from previous epoch
 
-    i = 0
-    while i < len(modified_events) - 1:
-        current_event = modified_events[i]
-        next_event = modified_events[i + 1]
-
-        if current_event.type == EventType.UNLOCK and next_event.type == EventType.LOCK:
-            if (
-                current_event.source == DepositSource.SABLIER
-                and next_event.source == DepositSource.OCTANT
-                and next_event.timestamp - current_event.timestamp
-                < sablier_unlock_grace_period
-            ):
-                unlocked_amount = current_event.amount
-                locked_amount = next_event.amount
-
-                if locked_amount == unlocked_amount:
-                    # Scenario 1: Transparent unlock and lock
-                    del modified_events[i : i + 2]
-                    continue
-                elif locked_amount > unlocked_amount:
-                    # Scenario 3: Transparent unlock, only record the excess lock
-                    excess_amount = locked_amount - unlocked_amount
-                    next_event.amount = excess_amount
-                    next_event.deposit_before = acc_balance_sablier + acc_balance_octant
-                    next_event.deposit_after = next_event.deposit_before + excess_amount
-                    del modified_events[i]  # Remove the unlock event
-                    continue
-
-        # Update balances for normal event processing
+    for event in modified_events[1:]:
         combined_balance = acc_balance_sablier + acc_balance_octant
-        current_event.deposit_before = combined_balance
+        event.deposit_before = combined_balance
 
-        if current_event.type == EventType.LOCK:
-            if current_event.source == DepositSource.SABLIER:
-                acc_balance_sablier += current_event.amount
+        if event.type == EventType.LOCK:
+            if event.source == DepositSource.SABLIER:
+                acc_balance_sablier += event.amount
             else:
-                acc_balance_octant += current_event.amount
+                acc_balance_octant += event.amount
 
-            current_event.deposit_after = (
-                current_event.deposit_before + current_event.amount
-            )
-        elif current_event.type == EventType.UNLOCK:
-            if current_event.source == DepositSource.SABLIER:
-                acc_balance_sablier -= current_event.amount
+            event.deposit_after = event.deposit_before + event.amount
+        elif event.type == EventType.UNLOCK:
+            if event.source == DepositSource.SABLIER:
+                acc_balance_sablier -= event.amount
             else:
-                acc_balance_octant -= current_event.amount
+                acc_balance_octant -= event.amount
 
-            current_event.deposit_after = (
-                current_event.deposit_before - current_event.amount
-            )
+            event.deposit_after = event.deposit_before - event.amount
 
-        i += 1
-
-    # Process the last event
-    if modified_events:
-        last_event = modified_events[-1]
-        combined_balance = acc_balance_sablier + acc_balance_octant
-        last_event.deposit_before = combined_balance
-        if last_event.type == EventType.LOCK:
-            last_event.deposit_after = last_event.deposit_before + last_event.amount
-        elif last_event.type == EventType.UNLOCK:
-            last_event.deposit_after = last_event.deposit_before - last_event.amount
-
-    return modified_events
+    modified_events_with_grace_period = _remove_redundant_events_within_grace_period(
+        modified_events, sablier_unlock_grace_period
+    )
+    print(modified_events_with_grace_period, flush=True)
+    return modified_events_with_grace_period
