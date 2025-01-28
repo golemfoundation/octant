7aa499f00f5e687c29c6023e7459da865187688f
adam-gf <adam@golem.foundation>
2025-01-28 01:56:35 +0100

    fix

--- backend/app/context/epoch/block_range.py
+++ backend/app/context/epoch/block_range.py
@@ -15,6 +15,4 @@ def get_blocks_range(
     start_block = get_block_num_from_ts(start_sec) if start_sec <= now_sec else None
     end_block = get_block_num_from_ts(end_sec) if end_sec <= now_sec else None
 
-    start_block = None
-    end_block = None
     return start_block, end_block
