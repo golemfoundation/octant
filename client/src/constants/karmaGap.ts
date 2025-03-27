98c495b7b71145ea1672b396faa692f33bd54d2e
Andrzej Ziółek <a.j.ziolek@gmail.com>
2025-03-27 11:03:10 +0100

    hotfix: Karma GAP new ids for programs after the change from their side

--- client/src/constants/karmaGap.ts
+++ client/src/constants/karmaGap.ts
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 // In Karma GAP our epochs are named "Programs".
 export const PROGRAMS_IDS_TO_EPOCH_NUMBER_MAPPING = {
-  1: '544',
-  2: '545',
-  3: '546',
-  4: '547',
-  5: '548',
-  6: '773',
-  7: '887',
+  1: '544_42161',
+  2: '545_42161',
+  3: '546_42161',
+  4: '547_42161',
+  5: '548_42161',
+  6: '773_42161',
+  7: '887_42161',
 };
 /* eslint-enable @typescript-eslint/naming-convention */
 
