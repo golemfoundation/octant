e35c270608649b66c6d30643ac08d7889f66b522
adam-gf <adam@golem.foundation>
2025-03-30 23:08:19 +0200

    fix: api tests for patron mode

--- backend/tests/api_e2e/test_api_patron_mode.py
+++ backend/tests/api_e2e/test_api_patron_mode.py
@@ -1,8 +1,9 @@
 import pytest
 
+from sqlalchemy.orm import Session
 from flask import current_app as app
 from tests.helpers.constants import STARTING_EPOCH
-from tests.conftest import UserAccount
+from tests.conftest import UserAccount, add_user_sync
 from tests.helpers.signature import build_user_signature_patron
 from tests.api_e2e.conftest import FastAPIClient
 
@@ -13,9 +14,11 @@ def test_patron_mode_basics(
     deployer: UserAccount,
     ua_alice: UserAccount,
     setup_funds,
+    sync_session: Session,
 ):
-    # lock GLM for one account
-    ua_alice.lock(10000)
+    # create user in db
+    add_user_sync(sync_session, ua_alice.address)
+    sync_session.commit()
 
     # wait for indexer to catch up
     fclient.wait_for_sync(STARTING_EPOCH)
@@ -33,9 +36,9 @@ def test_patron_mode_basics(
 
     # Check patron list after alice patron mode on
     patrons, status_code = fclient.get_epoch_patrons(STARTING_EPOCH)
-    assert len(patrons["patrons"]) == 1, "We should have only one patron"
-    for patron in patrons["patrons"]:
-        assert patron == ua_alice.address, "This parton should be alice"
+    assert (
+        len(patrons["patrons"]) == 0
+    ), "We have patron, but has no budget so not included"
     assert status_code == 200
 
     # Check Alice's patron mode
@@ -51,7 +54,7 @@ def test_patron_mode_basics(
 
     # Check patron list after alice patron mode off
     patrons, status_code = fclient.get_epoch_patrons(STARTING_EPOCH)
-    assert len(patrons["patrons"]) == 0, "There should be no patrons"
+    assert len(patrons["patrons"]) == 0, "There should be no patrons (still)"
     assert status_code == 200
 
     # Check Alice's patron mode
