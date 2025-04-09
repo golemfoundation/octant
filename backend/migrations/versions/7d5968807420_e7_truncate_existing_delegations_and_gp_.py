1a16a3a931bdca318245f8cc0f5afa33a886eded
Michał Kluczek <michal@wildland.io>
2025-04-10 00:44:37 +0200

    Fix non-linear alembic migration tree

--- backend/migrations/versions/7d5968807420_e7_truncate_existing_delegations_and_gp_.py
+++ backend/migrations/versions/7d5968807420_e7_truncate_existing_delegations_and_gp_.py
@@ -1,7 +1,7 @@
 """E7: Truncate existing delegations and gp stamps
 
 Revision ID: 7d5968807420
-Revises: c737027a131a
+Revises: 998d6bb80951
 Create Date: 2025-04-09 10:23:08.996921
 
 """
@@ -9,7 +9,7 @@ from alembic import op
 
 
 revision = "7d5968807420"
-down_revision = "c737027a131a"
+down_revision = "998d6bb80951"
 branch_labels = None
 depends_on = None
 
