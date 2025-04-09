dee227d45cad860d1980e12d577e35d863b822d6
Andrzej Ziółek <a.j.ziolek@gmail.com>
2025-04-10 00:05:16 +0200

    feat: another take

--- backend/app/infrastructure/routes/projects.py
+++ backend/app/infrastructure/routes/projects.py
@@ -65,7 +65,7 @@ class ProjectsMetadata(OctantResource):
 
         return {
             "projectsAddresses": projects_metadata.projects_addresses,
-            "projectsCid": projects_metadata.projects_cid,
+            "projectsCid": "bafybeifs53yk5oycvy5lu5r42oefk3vh7qkvfdkklkvaw2ocubmycgvche" if epoch == 7 else projects_metadata.projects_cid,
         }
 
 
