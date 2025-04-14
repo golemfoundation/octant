418d0f17fd238bf3368041fc967dd6a97384d13a
Andrzej Ziółek <a.j.ziolek@gmail.com>
2025-04-14 18:10:00 +0200

    feat: Projects & Project view - slice the list to 5 elements

--- client/src/components/Projects/ProjectsList/ProjectsList.tsx
+++ client/src/components/Projects/ProjectsList/ProjectsList.tsx
@@ -61,21 +61,23 @@ const ProjectsList: FC<ProjectsListProps> = ({
       )}
       <Grid>
         {projectsIpfsWithRewards.length > 0 && !isFetchingProjectsWithRewards
-          ? projectsIpfsWithRewardsSorted.map((projectIpfsWithRewards, index) => (
-              <ProjectsListItem
-                key={projectIpfsWithRewards.address}
-                dataTest={
-                  epoch
-                    ? `ProjectsView__ProjectsListItem--archive--${index}`
-                    : `ProjectsView__ProjectsListItem--${index}`
-                }
-                epoch={epoch}
-                isAnchorForTourguide={
-                  projectsIpfsWithRewardsSorted.length > 4 ? index === 5 : index === 0
-                }
-                projectIpfsWithRewards={projectIpfsWithRewards}
-              />
-            ))
+          ? projectsIpfsWithRewardsSorted
+              .slice(0, 5)
+              .map((projectIpfsWithRewards, index) => (
+                <ProjectsListItem
+                  key={projectIpfsWithRewards.address}
+                  dataTest={
+                    epoch
+                      ? `ProjectsView__ProjectsListItem--archive--${index}`
+                      : `ProjectsView__ProjectsListItem--${index}`
+                  }
+                  epoch={epoch}
+                  isAnchorForTourguide={
+                    projectsIpfsWithRewardsSorted.length > 4 ? index === 5 : index === 0
+                  }
+                  projectIpfsWithRewards={projectIpfsWithRewards}
+                />
+              ))
           : projectsEpoch?.projectsAddresses?.map((_, index) => (
               // eslint-disable-next-line react/no-array-index-key
               <ProjectsListSkeletonItem key={index} className={styles.element} />
