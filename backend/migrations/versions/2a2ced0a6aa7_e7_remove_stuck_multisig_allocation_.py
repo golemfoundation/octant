615d419d18671a12e17befcdfe313c10e3656e84
adam-gf <adam@golem.foundation>
2025-04-15 10:41:38 +0200

    adds status filter

--- backend/migrations/versions/2a2ced0a6aa7_e7_remove_stuck_multisig_allocation_.py
+++ backend/migrations/versions/2a2ced0a6aa7_e7_remove_stuck_multisig_allocation_.py
@@ -8,7 +8,6 @@ Create Date: 2025-04-15 10:27:44.341248
 from alembic import op
 
 
-# revision identifiers, used by Alembic.
 revision = "2a2ced0a6aa7"
 down_revision = "7d5968807420"
 branch_labels = None
@@ -16,7 +15,7 @@ depends_on = None
 
 
 def upgrade():
-    query = "DELETE FROM multisig_signatures WHERE id = 37 AND address = '0x25854e2a49A6CDAeC7f0505b4179834509038549' AND msg_hash = '0x8b4c12cb5bf3cd65331265011e4a8be45b35005fbc34692f20d52e0bb676fe57'"
+    query = "DELETE FROM multisig_signatures WHERE id = 37 AND address = '0x25854e2a49A6CDAeC7f0505b4179834509038549' AND msg_hash = '0x8b4c12cb5bf3cd65331265011e4a8be45b35005fbc34692f20d52e0bb676fe57' AND status = 'pending'"
     op.execute(query)
 
 
