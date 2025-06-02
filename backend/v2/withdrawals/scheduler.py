0985cba9b99ecc368d89cb31df21f37923566dd8
adam-gf <adam@golem.foundation>
2025-06-02 11:48:50 +0200

    adds scheduler + removes flask for testing

---
+++ backend/v2/withdrawals/scheduler.py
@@ -0,0 +1,27 @@
+from v2.core.dependencies import (
+    get_sessionmaker,
+    get_database_settings,
+    get_w3,
+    get_web3_provider_settings,
+)
+from v2.withdrawals.dependencies import get_vault_contracts, get_vault_settings
+from v2.withdrawals.services import confirm_withdrawals
+
+
+async def confirm_withdrawals_scheduled():
+    """
+    Confirm withdrawals for the current epoch.
+    This is a scheduled task that runs every 15 seconds.
+    """
+
+    print("confirm_withdrawals_scheduled called")
+    # Dependencies
+    db_settings = get_database_settings()
+    sessionmaker = get_sessionmaker(db_settings)
+    w3 = get_w3(get_web3_provider_settings())
+    vault_settings = get_vault_settings()
+    vault = get_vault_contracts(w3, vault_settings)
+
+    async with sessionmaker() as session:
+        print("confirm_withdrawals called")
+        await confirm_withdrawals(session, vault)
