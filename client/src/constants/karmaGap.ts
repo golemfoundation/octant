b1395fee3f18e4eb8dace64ffe01dfb3c5856ce8
Andrzej Ziółek <a.j.ziolek@gmail.com>
2025-06-25 23:13:45 +0200

    feat: karma gap epoch 8 aw id

--- client/src/constants/karmaGap.ts
+++ client/src/constants/karmaGap.ts
@@ -8,6 +8,7 @@ export const PROGRAMS_IDS_TO_EPOCH_NUMBER_MAPPING = {
   5: '548_42161',
   6: '773_42161',
   7: '887_42161',
+  8: '945_42161',
 };
 /* eslint-enable @typescript-eslint/naming-convention */
 
