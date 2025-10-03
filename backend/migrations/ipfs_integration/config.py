ae638fd6a235ae21c1f148c6c897a5605dce4455
kgarbacinski <kacpergarbacinski@gmail.com>
2025-10-03 11:59:10 +0200

    feat: update projects from ipfs

--- backend/migrations/ipfs_integration/config.py
+++ backend/migrations/ipfs_integration/config.py
@@ -5,7 +5,7 @@ from core import build_filename
 
 class Config:
     FILENAME_PREFIX = "ipfs_projects_details"
-    EPOCH = 8  # change corresponding to the epoch
+    EPOCH = 9  # change corresponding to the epoch
     JSON_FILEPATH = f"files/{build_filename(FILENAME_PREFIX, EPOCH)}"
     CID = [
         "bafybeibunwwuo3edrfi7y2jh3bsedwskjxzik7jcjk7bbxqejjqj244prq"
