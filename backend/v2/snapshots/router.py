ed99bf890002c7d9cdd2f7e570a3783f60344e58
Zhaolie <zhaolie_of_han@protonmail.com>
2025-10-07 13:40:55 +0100

    chore: fix comment typo

--- backend/v2/snapshots/router.py
+++ backend/v2/snapshots/router.py
@@ -393,7 +393,7 @@ async def simulate_finalized_snapshot_v1(
         all_projects,
     )
 
-    # How much each user took for themselfs (non zero only)
+    # How much each user took for themselves (non zero only)
     # How much was allocated towards projects
     # How much was distibuted in total
     user_claimed_rewards = await get_all_users_claimed_rewards(session, epoch_number)
