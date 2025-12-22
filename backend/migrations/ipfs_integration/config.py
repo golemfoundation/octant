a9f4a2d7d686ed9750d96c277b4c4aa807566633
Andrzej Ziółek <a.j.ziolek@gmail.com>
2025-12-23 00:28:56 +0100

    feat: epoch number in config

--- backend/migrations/ipfs_integration/config.py
+++ backend/migrations/ipfs_integration/config.py
@@ -5,7 +5,7 @@ from core import build_filename
 
 class Config:
     FILENAME_PREFIX = "ipfs_projects_details"
-    EPOCH = 9  # change corresponding to the epoch
+    EPOCH = 10  # change corresponding to the epoch
     JSON_FILEPATH = f"files/{build_filename(FILENAME_PREFIX, EPOCH)}"
     CID = [
         "bafybeief7d3g5t6mqwmbdvt32dsztzzanidu5qynwdzwsurbebs4xqhamu"
