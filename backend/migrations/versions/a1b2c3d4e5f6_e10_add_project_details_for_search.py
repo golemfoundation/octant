b55d33e67eadafbee6dad661f4d3a6ddf344c7d2
Andrzej Ziółek <a.j.ziolek@gmail.com>
2025-12-22 23:45:58 +0100

    style: format

--- backend/migrations/versions/a1b2c3d4e5f6_e10_add_project_details_for_search.py
+++ backend/migrations/versions/a1b2c3d4e5f6_e10_add_project_details_for_search.py
@@ -7,6 +7,7 @@ Create Date: 2025-12-22 11:31:29.147255
 """
 from migrations.ipfs_integration import migration_helpers as ipfs_migration
 
+# revision identifiers, used by Alembic.
 revision = "a1b2c3d4e5f6"
 down_revision = "40dec3f9c59c"
 branch_labels = None
@@ -15,9 +16,11 @@ depends_on = None
 FILENAME = "ipfs_projects_details_epoch_{}.json"
 EPOCH = 10
 
+
 def upgrade():
     filename = FILENAME.format(EPOCH)
     ipfs_migration.upgrade(filename, EPOCH)
+
 
 def downgrade():
     filename = FILENAME.format(EPOCH)
