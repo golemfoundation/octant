78786b28975a1753d2493df763b7792e37e3e361
Andrzej Ziółek <a.j.ziolek@gmail.com>
2026-01-28 15:01:37 +0100

    fix: Allocation.tsx migration flag

--- client/src/components/Allocation/Allocation.tsx
+++ client/src/components/Allocation/Allocation.tsx
@@ -67,7 +67,7 @@ const Allocation: FC<AllocationProps> = ({ dataTest }) => {
   const [percentageProportions, setPercentageProportions] = useState<PercentageProportions>({});
   const { data: projectsEpoch } = useProjectsEpoch();
   const { data: projectsIpfsWithRewards } = useProjectsIpfsWithRewards();
-  const { isUserMigrationRequired } = useIsUserMigrationDoneOrRequired();
+  const { data: { isUserMigrationRequired } } = useIsUserMigrationDoneOrRequired();
   const [isModalMigrateOpen, setIsModalMigrateOpen] = useState<boolean>(false);
   const isInMigrationMode = useIsMigrationMode();
   const { isRewardsForProjectsSet } = useAllocationViewSetRewardsForProjects();
