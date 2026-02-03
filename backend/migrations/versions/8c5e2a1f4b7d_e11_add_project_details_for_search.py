f7b8b5667ff555e3ebe99f958f4e259689c660ef
Andrzej Ziółek <a.j.ziolek@gmail.com>
2026-02-03 15:09:08 +0100

    fix: revision time

--- backend/migrations/versions/8c5e2a1f4b7d_e11_add_project_details_for_search.py
+++ backend/migrations/versions/8c5e2a1f4b7d_e11_add_project_details_for_search.py
@@ -2,7 +2,7 @@
 
 Revision ID: 8c5e2a1f4b7d
 Revises: e2f4c1a7b8d9
-Create Date: 2025-12-22 11:31:29.147255
+Create Date: 2026-02-03 13:36:41.642611
 
 """
 from migrations.ipfs_integration import migration_helpers as ipfs_migration
